00040b0724314ab67a2862e3b6a57a914d7ca426
bg1bgst333 <bg1bgst333@gmail.com>
2020-05-27 15:12:59 +0900

    add CApplication

---
+++ winapi/CApplication/CApplication/src/CApplication/CApplication/Application.h
@@ -0,0 +1,26 @@
+// 二重インクルード防止
+#ifndef __APPLICATION_H__
+#define __APPLICATION_H__
+
+// ヘッダのインクルード
+// 既定のヘッダ
+#include <windows.h>	// 標準WindowsAPI
+#include <tchar.h>		// TCHAR型
+
+// アプリケーションクラスCApplicationの定義
+class CApplication{
+
+	// publicメンバ
+	public:
+
+		// publicメンバ巻数
+		virtual BOOL InitInstance(HINSTANCE hInstance, LPTSTR lpCmdLine, int nShowCmd);	// インスタンス初期化関数InitInstance.
+		virtual int Run();	// メッセージループ処理関数Run.
+		virtual int ExitInstance();	// 終了処理関数ExitInstance.
+
+};
+
+// 関数のプロトタイプ宣言
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);	// ウィンドウプロシージャWindowProc.
+
+#endif
