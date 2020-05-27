cad723dcb9e5d83119cc9b3a91109ce65b62793b
bg1bgst333 <bg1bgst333@gmail.com>
2020-05-27 23:20:52 +0900

    add CWindow::StaticWindowProc

---
+++ winapi/CWindow/StaticWindowProc/src/CWindow/CWindow/Window.h
@@ -0,0 +1,32 @@
+// 二重インクルード防止
+#ifndef __WINDOW_H__
+#define __WINDOW_H__
+
+// ヘッダのインクルード
+// 既定のヘッダ
+#include <windows.h>	// 標準WindowsAPI
+#include <tchar.h>		// TCHAR型
+
+// ウィンドウクラスCWindowの定義
+class CWindow{
+
+	// publicメンバ
+	public:
+
+		// publicメンバ変数
+		HWND m_hWnd;	// HWND型ウィンドウハンドルm_hWnd.
+
+		// publicメンバ関数
+		// コンストラクタ・デストラクタ
+		CWindow();	// コンストラクタCWindow()
+		virtual ~CWindow();	// デストラクタ~CWindow()
+		// staticメンバ関数
+		static BOOL RegisterClass(HINSTANCE hInstance, LPCTSTR lpctszClassName);	// ウィンドウクラス登録関数RegisterClass
+		static LRESULT CALLBACK StaticWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);	// スタティックウィンドウプロシージャStaticWindowProc.
+		// メンバ関数
+		virtual BOOL Create(LPCTSTR lpctszClassName, LPCTSTR lpctszWindowName, DWORD dwStyle, int x, int y, int iWidth, int iHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance);	// ウィンドウ作成関数Create.
+		virtual BOOL ShowWindow(int nCmdShow);	// ウィンドウ表示関数ShowWindow.
+
+};
+
+#endif
