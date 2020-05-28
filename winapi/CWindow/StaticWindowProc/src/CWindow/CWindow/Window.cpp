51a60e49e6e596c7a2e1df56c000be372376d9a5
B.G <bg1bgst333@gmail.com>
2020-05-29 01:15:25 +0900

    add CWindow::StaticWindowProc

---
+++ winapi/CWindow/StaticWindowProc/src/CWindow/CWindow/Window.cpp
@@ -0,0 +1,125 @@
+// ヘッダのインクルード
+// 独自ヘッダ
+#include "Window.h"	// CWindow
+
+// コンストラクタCWindow()
+CWindow::CWindow() {
+
+	// メンバの初期化.
+	m_hWnd = NULL;	// m_hWndをNULLで初期化.
+
+}
+
+// デストラクタ~CWindow()
+CWindow::~CWindow() {
+
+	// メンバの終了処理.
+	if (m_hWnd != NULL) {	// m_hWndがNULLでない時.
+		DestroyWindow(m_hWnd);	// DestroyWindowでm_hWndを破棄.
+		m_hWnd = NULL;	// m_hWndにNULLをセット.
+	}
+
+}
+
+// ウィンドウクラス登録関数RegisterClass.
+BOOL CWindow::RegisterClass(HINSTANCE hInstance, LPCTSTR lpctszClassName) {
+
+	// 変数・構造体の宣言
+	WNDCLASS wc;	// WNDCLASS型ウィンドウクラス構造体wc.
+
+	// ウィンドウクラス構造体wcにパラメータをセット.
+	wc.lpszClassName = lpctszClassName;	// ウィンドウクラス名にlpctszClassNameをセットする.
+	wc.style = CS_HREDRAW | CS_VREDRAW;	// スタイルはとりあえずCS_HREDRAW | CS_VREDRAWとする.
+	wc.lpfnWndProc = StaticWindowProc;	// ウィンドウプロシージャには下で定義するStaticWindowProcを指定する.
+	wc.hInstance = hInstance;	// アプリケーションインスタンスハンドルは引数のhInstanceを使う.
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);	// LoadIconでアプリケーション既定のアイコンをロード.
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);	// LoadCursorでアプリケーション既定のカーソルをロード.
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);	// GetStockObjectで白ブラシを背景とする.
+	wc.lpszMenuName = NULL;	// メニューはなし, なのでNULL.
+	wc.cbClsExtra = 0;	// とりあえず0を指定.
+	wc.cbWndExtra = 0;	// とりあえず0を指定.
+
+	// ウィンドウクラスの登録.
+	if (!::RegisterClass(&wc)) {	// WindowsAPIのRegisterClassでウィンドウクラスを登録する.
+
+		// 戻り値が0なら登録失敗なのでエラー処理.
+		return FALSE;	// FALSEを返す.
+
+	}
+
+	// 登録成功なのでTRUE.
+	return TRUE;	// returnでTRUEを返す.
+
+}
+
+// スタティックウィンドウプロシージャStaticWindowProc関数の定義
+LRESULT CALLBACK CWindow::StaticWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+
+	// ウィンドウメッセージの処理.
+	switch (uMsg) {	// uMsgの値ごとに処理を振り分ける.
+
+		// ウィンドウの作成が開始された時.
+		case WM_CREATE:
+
+			// WM_CREATEブロック
+			{
+
+				// ウィンドウ作成を成功とさせるには0を返す.
+				return 0;	// 0を返す.
+
+			}
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// ウィンドウが破棄された時.
+		case WM_DESTROY:
+
+			// WM_DESTROYブロック
+			{
+
+				// アプリケーションを終了させたいので, 終了メッセージの送信.
+				PostQuitMessage(0);	// PostQuitMessageで終了コードを0として, WM_QUITメッセージを送信.
+
+			}
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// 上記以外の時.
+		default:
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+	}
+
+	// あとは既定の処理に任せる.
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);	// 戻り値も含めてDefWindowProcに既定の処理を任せる.
+
+}
+
+// ウィンドウ作成関数Create.
+BOOL CWindow::Create(LPCTSTR lpctszClassName, LPCTSTR lpctszWindowName, DWORD dwStyle, int x, int y, int iWidth, int iHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance) {
+
+	// ウィンドウの作成.
+	m_hWnd = CreateWindow(lpctszClassName, lpctszWindowName, dwStyle, x, y, iWidth, iHeight, hWndParent, hMenu, hInstance, NULL);	// CreateWindowでウィンドウを作成し, ハンドルをm_hWndに格納.(最後の引数はとりあえずNULL.)
+	if (m_hWnd == NULL) {	// hWndがNULLならウィンドウ作成失敗.
+
+		// hWndがNULLならウィンドウ作成失敗なのでエラー処理.
+		return FALSE;	// FALSEを返す.
+
+	}
+
+	// ウィンドウ作成成功なのでTRUE.
+	return TRUE;	// returnでTRUEを返す.
+
+}
+
+// ウィンドウ表示関数ShowWindow.
+BOOL CWindow::ShowWindow(int nCmdShow) {
+
+	// ウィンドウ表示.
+	return ::ShowWindow(m_hWnd, nCmdShow);	// WindowsAPIのShowWindowでm_hWndを表示.
+
+}
