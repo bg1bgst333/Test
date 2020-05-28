d5e6a261069312cf8b74e6d49e3d9aa32ddd0d9d
B.G <bg1bgst333@gmail.com>
2020-05-28 18:39:46 +0900

    add CWindow

---
+++ winapi/CWindow/CWindow/src/CWindow/CWindow/Window.h
@@ -0,0 +1,34 @@
+// 二重インクルード防止
+#ifndef __WINDOW_H__
+#define __WINDOW_H__
+
+// ヘッダのインクルード
+// 既定のヘッダ
+#include <windows.h>	// 標準WindowsAPI
+#include <tchar.h>		// TCHAR型
+
+// ウィンドウクラスCWindowの定義
+class CWindow {
+
+	// publicメンバ
+	public:
+
+		// publicメンバ変数
+		HWND m_hWnd;	// HWND型ウィンドウハンドルm_hWnd.
+
+		// publicメンバ関数
+		// コンストラクタ・デストラクタ
+		CWindow();	// コンストラクタCWindow()
+		virtual ~CWindow();	// デストラクタ~CWindow()
+		// staticメンバ関数
+		static BOOL RegisterClass(HINSTANCE hInstance, LPCTSTR lpctszClassName);	// ウィンドウクラス登録関数RegisterClass
+		// メンバ関数
+		virtual BOOL Create(LPCTSTR lpctszClassName, LPCTSTR lpctszWindowName, DWORD dwStyle, int x, int y, int iWidth, int iHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance);	// ウィンドウ作成関数Create.
+		virtual BOOL ShowWindow(int nCmdShow);	// ウィンドウ表示関数ShowWindow.
+
+};
+
+// 関数のプロトタイプ宣言
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);	// ウィンドウプロシージャWindowProc.
+
+#endif
