60f3474796cb9581ea1f8f192c29d9fe53c7d0f8
B.G <bg1bgst333@gmail.com>
2021-01-23 20:11:59 +0900

    add CWindow::DynamicWindowProc

---
+++ winapi/CWindow/DynamicWindowProc/src/CWindow/CWindow/Application.h
@@ -0,0 +1,25 @@
+// 二重インクルード防止
+#ifndef __APPLICATION_H__
+#define __APPLICATION_H__
+
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "Window.h"	// CWindow
+
+// アプリケーションクラスCApplicationの定義
+class CApplication {
+
+	// publicメンバ
+	public:
+
+		// publicメンバ変数
+		CWindow m_mainWnd;	// メインウィンドウm_mainWnd.
+
+		// publicメンバ関数
+		virtual BOOL InitInstance(HINSTANCE hInstance, LPTSTR lpCmdLine, int nShowCmd);	// インスタンス初期化関数InitInstance.
+		virtual int Run();	// メッセージループ処理関数Run.
+		virtual int ExitInstance();	// 終了処理関数InitInstance.
+
+};
+
+#endif
