9e4e89e2ce1c096f27483f5ce8c45c4c9ffc7e53
bg1bgst333 <bg1bgst333@gmail.com>
2022-01-29 14:20:38 +0900

    add CWindow::RegisterClass

---
+++ winapi/CWindow/RegisterClass/src/CWindow/CWindow/MainWindow.cpp
@@ -0,0 +1,19 @@
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "MainWindow.h"	// CMainWindow
+
+// ウィンドウクラス登録関数RegisterClass.
+BOOL CMainWindow::RegisterClass(HINSTANCE hInstance){
+
+	// ウィンドウクラス名は"CMainWindow".
+	return CWindow::RegisterClass(hInstance, _T("CMainWindow"));	// CWindow::RegisterClassでウィンドウクラス名"CMainWindow"を登録.
+
+}
+
+// ウィンドウ作成関数Create.(ウィンドウクラス名省略バージョン.)
+BOOL CMainWindow::Create(LPCTSTR lpctszWindowName, DWORD dwStyle, int x, int y, int iWidth, int iHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance){
+
+	// ウィンドウクラス名は"CMainWindow".
+	return CWindow::Create(_T("CMainWindow"), lpctszWindowName, dwStyle, x, y, iWidth, iHeight, hWndParent, hMenu, hInstance);	// CWindow::Createにウィンドウクラス名"CMainWindow"を指定.
+
+}
---
+++ winapi/CWindow/RegisterClass/src/CWindow/CWindow/MainWindow.h
@@ -0,0 +1,22 @@
+// 二重インクルード防止
+#ifndef __MAIN_WINDOW_H__
+#define __MAIN_WINDOW_H__
+
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "Window.h"	// CWindow
+
+// メインウィンドウクラスCMainWindow
+class CMainWindow : public CWindow{
+
+	// publicメンバ
+	public:
+
+		// staticメンバ関数
+		static BOOL RegisterClass(HINSTANCE hInstance);	// ウィンドウクラス登録関数RegisterClass.
+		// メンバ関数
+		virtual BOOL Create(LPCTSTR lpctszWindowName, DWORD dwStyle, int x, int y, int iWidth, int iHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance);	// ウィンドウ作成関数Create.(ウィンドウクラス名省略バージョン.)
+
+};
+
+#endif
