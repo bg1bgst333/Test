c70f8042b9999682f2840bf4f58ab580007168f8
B.G <bg1bgst333@gmail.com>
2022-02-01 11:53:51 +0900

    add CWindow::OnDestroy

---
+++ winapi/CWindow/OnDestroy/src/CWindow/CWindow/MainWindow.h
@@ -0,0 +1,24 @@
+// 二重インクルード防止
+#ifndef __MAIN_WINDOW_H__
+#define __MAIN_WINDOW_H__
+
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "Window.h"	// CWindow
+
+// メインウィンドウクラスCMainWindow
+class CMainWindow : public CWindow {
+
+	// publicメンバ
+	public:
+
+		// staticメンバ関数
+		static BOOL RegisterClass(HINSTANCE hInstance);	// ウィンドウクラス登録関数RegisterClass.
+		// メンバ関数
+		virtual BOOL Create(LPCTSTR lpctszWindowName, DWORD dwStyle, int x, int y, int iWidth, int iHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance);	// ウィンドウ作成関数Create.(ウィンドウクラス名省略バージョン.)
+		virtual int OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct);	// ウィンドウの作成が開始された時.
+		virtual void OnDestroy();	// ウィンドウが破棄された時.
+
+};
+
+#endif
