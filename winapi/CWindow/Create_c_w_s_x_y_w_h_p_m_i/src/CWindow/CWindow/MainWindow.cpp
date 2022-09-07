685d5cad8788a716eb83a9cc1784e4d1f47c1056
B.G <bg1bgst333@gmail.com>
2022-09-07 15:23:01 +0900

    add CWindow::Create_c_w_s_x_y_w_h_p_m_i

---
+++ winapi/CWindow/Create_c_w_s_x_y_w_h_p_m_i/src/CWindow/CWindow/MainWindow.cpp
@@ -0,0 +1,11 @@
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "MainWindow.h"	// CMainWindow
+
+// ウィンドウ作成関数Create.(ウィンドウクラス名省略バージョン.)
+BOOL CMainWindow::Create(LPCTSTR lpctszWindowName, DWORD dwStyle, int x, int y, int iWidth, int iHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance) {
+
+	// ウィンドウクラス名は"CMainWindow".
+	return CWindow::Create(_T("CMainWindow"), lpctszWindowName, dwStyle, x, y, iWidth, iHeight, hWndParent, hMenu, hInstance);	// CWindow::Createにウィンドウクラス名"CMainWindow"を指定.
+
+}
