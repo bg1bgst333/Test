4f4f1d4d67d51196d2774c101cc441d620d3f553
B.G <bg1bgst333@gmail.com>
2022-09-14 14:40:16 +0900

    add CStaticCore::Create

---
+++ winapi/CStaticCore/Create/src/CStaticCore/CStaticCore/StaticCore.h
@@ -0,0 +1,21 @@
+// 二重インクルード防止
+#ifndef __STATIC_CORE_H__
+#define __STATIC_CORE_H__
+
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "CustomControl.h"	// CCustomControl
+
+// スタティックコアコントロールクラスCStaticCore
+class CStaticCore : public CCustomControl {
+
+	// publicメンバ
+	public:
+
+		// publicメンバ関数
+		// メンバ関数
+		virtual BOOL Create(LPCTSTR lpctszWindowName, DWORD dwStyle, int x, int y, int iWidth, int iHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance);	// ウィンドウ作成関数Create.
+
+};
+
+#endif
