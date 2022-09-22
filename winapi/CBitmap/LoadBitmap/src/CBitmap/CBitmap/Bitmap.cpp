880ce2e9b5c0f45f8f2e7344977b84926383ca05
bg1bgst333 <bg1bgst333@gmail.com>
2022-09-22 12:23:19 +0900

    add CBitmap::LoadBitmap

---
+++ winapi/CBitmap/LoadBitmap/src/CBitmap/CBitmap/Bitmap.cpp
@@ -0,0 +1,36 @@
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "Bitmap.h"	// CBitmap
+
+// コンストラクタCBitmap()
+CBitmap::CBitmap(){
+
+	// メンバの初期化.
+	m_hBitmap = NULL;	// m_hBitmapをNULLで初期化.
+
+}
+
+// デストラクタ~CBitmap()
+CBitmap::~CBitmap(){
+
+	// メンバの終了処理.
+	if (m_hBitmap != NULL){	// NULLでない.
+		::DeleteObject(m_hBitmap);	// DeleteObjectで破棄.
+		m_hBitmap = NULL;	// NULLをセット.
+	}
+
+}
+
+// ビットマップリソースロード関数LoadBitmap.
+BOOL CBitmap::LoadBitmap(HINSTANCE hInstance, LPCTSTR lpctszResourceName){
+
+	// ビットマップリソースをロード.
+	m_hBitmap = ::LoadBitmap(hInstance, lpctszResourceName);	// LoadBitmapで指定のビットマップリソースをロード.
+	if (m_hBitmap != NULL){	// NULLでない.
+		return TRUE;	// TRUEを返す.
+	}
+	else{	// NULL.
+		return FALSE;	// FALSEを返す.
+	}
+
+}
---
+++ winapi/CBitmap/LoadBitmap/src/CBitmap/CBitmap/resource.h
@@ -0,0 +1,3 @@
+#define IDR_MAINMENU                    101
+#define ID_ITEM_1_1                     10101
+#define IDB_BITMAP1                     102
