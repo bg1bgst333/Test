059007d681f30bd0cc2b3b2617d9d1ca87d97c15
bg1bgst333 <bg1bgst333@gmail.com>
2022-09-28 10:58:21 +0900

    add CStaticCore::SetBitmap

---
+++ winapi/CStaticCore/SetBitmap/src/CStaticCore/CStaticCore/MainWindow.cpp
@@ -0,0 +1,160 @@
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "MainWindow.h"	// CMainWindow
+
+// ウィンドウクラス登録関数RegisterClass.
+BOOL CMainWindow::RegisterClass(HINSTANCE hInstance){
+
+	// ウィンドウクラス名は"CMainWindow".
+	return CWindow::RegisterClass(hInstance, _T("CMainWindow"));	// CWindow::RegisterClassでウィンドウクラス名"CMainWindow"を登録.
+
+}
+
+// ウィンドウクラス登録関数RegisterClass.(メニュー名指定バージョン)
+BOOL CMainWindow::RegisterClass(HINSTANCE hInstance, LPCTSTR lpctszMenuName){
+
+	// メニュー名はlpctszMenuName.
+	return CWindow::RegisterClass(hInstance, _T("CMainWindow"), lpctszMenuName);	// CWindow::RegisterClassで, ウィンドウクラス名"CMainWindow", メニュー名lpctszMenuNameを登録.
+
+}
+
+// コンストラクタCMainWindow()
+CMainWindow::CMainWindow(){
+
+	// メンバの初期化.
+	m_pStatic = NULL;	// m_pStaticをNULLで初期化.
+	m_pBitmap = NULL;	// m_pBitmapをNULLで初期化.
+
+
+}
+
+// デストラクタ~CMainWindow()
+CMainWindow::~CMainWindow(){
+
+	// メンバの終了処理.
+	Destroy();	// Destroyで子ウィンドウの破棄.
+
+}
+
+// ウィンドウ作成関数Create.(ウィンドウクラス名省略バージョン.)
+BOOL CMainWindow::Create(LPCTSTR lpctszWindowName, DWORD dwStyle, int x, int y, int iWidth, int iHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance){
+
+	// ウィンドウクラス名は"CMainWindow".
+	return CWindow::Create(_T("CMainWindow"), lpctszWindowName, dwStyle, x, y, iWidth, iHeight, hWndParent, hMenu, hInstance);	// CWindow::Createにウィンドウクラス名"CMainWindow"を指定.
+
+}
+
+// ウィンドウ破棄関数Destroy
+BOOL CMainWindow::Destroy(){
+
+	// 変数の初期化.
+	BOOL bRet = FALSE;	// bRetをFALSEで初期化.
+
+	// DestroyChildrenを分けたので, 自身のウィンドウ破棄は問題ない.
+	// まず子ウィンドウの破棄.
+	DestroyChildren();
+
+	// 自身のウィンドウ破棄.
+	bRet = CWindow::Destroy();	// 戻り値をbRetに格納.
+
+	// bRetを返す.
+	return bRet;
+
+}
+
+// 子ウィンドウ破棄関数DestroyChildren
+BOOL CMainWindow::DestroyChildren(){
+
+	// 変数の初期化.
+	BOOL bRet = FALSE;	// bRetをFALSEで初期化.
+
+	// ビットマップオブジェクトの破棄.
+	if (m_pBitmap != NULL){	// NULLでなければ.
+		delete m_pBitmap;	// deleteでm_pBitmapを解放.
+		m_pBitmap = NULL;	// NULLをセット.
+	}
+
+	// 子ウィンドウの破棄.
+	if (m_pStatic != NULL) {	// NULLでなければ.
+		bRet = m_pStatic->Destroy();	// m_pStatic->Destroyでウィンドウを破棄.
+		delete m_pStatic;	// deleteでm_pStaticを解放.
+		m_pStatic = NULL;	// NULLをセット.
+	}
+
+	// 破棄したらTRUEを返す.
+	if (bRet) {	// TRUEなら.
+		return TRUE;	// TRUEを返す.
+	}
+
+	// 破棄しなければ, CWindowのDestroyChildrenを返す.
+	return CWindow::DestroyChildren();	// CWindow::DestroyChildrenを返す.
+
+}
+
+// ウィンドウの作成が開始された時.
+int CMainWindow::OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct){
+
+	// メニューハンドラの追加.
+	AddCommandHandler(ID_ITEM_1_1, 0, (int(CWindow::*)(WPARAM, LPARAM))&CMainWindow::OnItem1_1);	// AddCommandHandlerでID_ITEM_1_1に対するハンドラCMainWindow::OnItem1_1を登録.
+
+	// スタティックコアコントロールオブジェクトの作成.
+	m_pStatic = new CStaticCore();	// newでCStaticCoreオブジェクトを作成し, ポインタをm_pStaticに格納.
+
+	// スタティックコアコントロールのウィンドウ作成.
+	RECT rc;	// RECT構造体rc.
+	rc.left = 50;		// 左50
+	rc.right = 150;		// 右150
+	rc.top = 50;		// 上50
+	rc.bottom = 150;	// 下150
+	m_pStatic->Create(_T(""), WS_BORDER | SS_BITMAP, rc, hwnd, (HMENU)(WM_APP + 1), lpCreateStruct->hInstance);	// Createでスタティックコアコントロールのウィンドウ作成.
+	
+	// ビットマップオブジェクトの作成とロード.
+	m_pBitmap = new CBitmap();	// newでCBitmapオブジェクトを作成し, ポインタをm_pBitmapに格納.
+	m_pBitmap->LoadBitmap(lpCreateStruct->hInstance, MAKEINTRESOURCE(IDB_BITMAP1));	// // CBitmap::LoadBitmapでIDB_BITMAP1をロード.
+
+	// スタティックコントロールにビットマップをセット.
+	m_pStatic->SetBitmap(*m_pBitmap);	// CStatic::SetBitmapで*m_pBitmapをセット.
+	
+	// 親クラスのOnCreateを呼ぶ.
+	return CWindow::OnCreate(hwnd, lpCreateStruct);	// CWindow::OnCreateを呼び, 戻り値を返す.
+
+}
+
+// ウィンドウが破棄された時.
+void CMainWindow::OnDestroy(){
+
+	// メニューハンドラの削除.
+	DeleteCommandHandler(ID_ITEM_1_1, 0);	// DeleteCommandHandlerでID_ITEM_1_1のハンドラを削除.
+
+	// CWindowのOnDestroyを呼ぶ.
+	CWindow::OnDestroy();	// CWindow::OnDestroyを呼ぶ.
+
+}
+
+// ウィンドウが閉じられる時.
+int CMainWindow::OnClose(){
+
+	// メッセージボックスで"Close CMainWindow OK?"と表示.
+	int iRet = MessageBox(m_hWnd, _T("Close CMainWindow OK?"), _T("CStaticCore"), MB_OKCANCEL);	// MessageBoxで"Close CMainWindow OK?"と表示し, 戻り値をiRetに格納.
+	if (iRet != IDOK){	// OK以外.(Cancelなど.)
+		return -1;	// -1を返す.
+	}
+
+	// このウィンドウの破棄.(OnCloseの後, ウィンドウの破棄処理が勝手に行われるので, Destroyは不要なのでコメントアウト.)
+	//Destroy();	// Destroyでこのウィンドウの破棄処理.
+
+	// OKなので閉じる.
+	return CWindow::OnClose();	// 親クラスのOnCloseを呼ぶ.(親クラスのOnCloseは常に閉じる処理になっている.)
+
+}
+
+// Item1-1が選択された時.
+int CMainWindow::OnItem1_1(WPARAM wParam, LPARAM lParam){
+
+	// スタティックコントロールのウィンドウテキストを変更する.
+	m_pStatic->SetWindowText(_T("Renamed!"));	// CWindow::SetWindowTextで"Renamed!"に変更.
+
+	// 0を返す.
+	return 0;	// 処理したので0.
+
+}
---
+++ winapi/CStaticCore/SetBitmap/src/CStaticCore/CStaticCore/StaticCore.cpp
@@ -0,0 +1,27 @@
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "StaticCore.h"	// CStaticCore
+
+// ウィンドウ作成関数Create.
+BOOL CStaticCore::Create(LPCTSTR lpctszWindowName, DWORD dwStyle, int x, int y, int iWidth, int iHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance){
+
+	// ウィンドウクラス名には"Static"を指定.
+	return CCustomControl::Create(_T("Static"), lpctszWindowName, dwStyle, x, y, iWidth, iHeight, hWndParent, hMenu, hInstance);	// CCustomControl::Createにウィンドウクラス名"Static"を指定.
+
+}
+
+// ウィンドウ作成関数Create(RECTバージョン).
+BOOL CStaticCore::Create(LPCTSTR lpctszWindowName, DWORD dwStyle, const RECT& rect, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance){
+
+	// ウィンドウクラス名には"Static"を指定.
+	return CCustomControl::Create(_T("Static"), lpctszWindowName, dwStyle, rect, hWndParent, hMenu, hInstance);	// CCustomControl::Createにウィンドウクラス名"Static"を指定.
+
+}
+
+// ビットマップのセットSetBitmap.
+void CStaticCore::SetBitmap(HBITMAP hBitmap){
+
+	// スタティックコントロールにビットマップをセット.
+	SendMessage(m_hWnd, STM_SETIMAGE, (WPARAM)IMAGE_BITMAP, (LPARAM)hBitmap);	// SendMessageのSTM_SETIMAGEでm_hWndにhBitmapをセット.
+
+}
