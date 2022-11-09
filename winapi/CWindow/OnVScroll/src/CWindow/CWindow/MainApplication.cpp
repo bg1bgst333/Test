c4e024395eee7915f88e2dba9615779cf6199f77
miura <miura@divakk.co.jp>
2022-11-09 09:40:34 +0900

    fixed CWindow::OnVScroll

--- winapi/CWindow/OnVScroll/src/CWindow/CWindow/MainApplication.cpp
+++ winapi/CWindow/OnVScroll/src/CWindow/CWindow/MainApplication.cpp
@@ -13,7 +13,7 @@ BOOL CMainApplication::InitInstance(HINSTANCE hInstance, LPTSTR lpCmdLine, int n
 	m_pMainWnd = new CMainWindow();	// CMainWindowオブジェクトを作成し, m_pMainWndに格納.
 
 	// ウィンドウの作成.
-	if (!m_pMainWnd->Create(_T("CWindow"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance)) {	// m_pMainWnd->Createでウィンドウ作成し, 失敗した場合.
+	if (!m_pMainWnd->Create(_T("CWindow"), WS_OVERLAPPEDWINDOW | WS_VSCROLL, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance)) {	// m_pMainWnd->Createでウィンドウ作成し, 失敗した場合.
 
 		// エラー処理
 		return FALSE;	// returnでFALSEを返して異常終了.
