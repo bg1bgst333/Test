1ebd0be9c6d3c2e9458773cfb4aea4adfe4e5b8b
bg1bgst333 <bg1bgst333@gmail.com>
2022-11-12 11:31:30 +0900

    add CPicture::CPicture_1

---
+++ winapi/CPicture/CPicture_1/src/CPicture/CPicture/CustomControl.cpp
@@ -0,0 +1,357 @@
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "CustomControl.h"	// CCustomControl
+
+// staticメンバ変数の定義
+std::map<tstring, WNDPROC>	CCustomControl::m_mapBaseWindowProcMap;	// ベースウィンドウプロシージャマップm_mapBaseWindowProcMap
+
+// コンストラクタCCustomControl
+CCustomControl::CCustomControl() : CWindow(){
+
+}
+
+// デストラクタ~CCustomControl
+CCustomControl::~CCustomControl(){
+
+}
+
+// スタティックウィンドウプロシージャStaticWindowProc.
+LRESULT CALLBACK CCustomControl::StaticWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
+
+	// ポインタの初期化
+	CWindow *pWindow = NULL;	// CWindowオブジェクトポインタpWindowをNULLに初期化.
+
+	// hwndでCWindowオブジェクトポインタが引けたら, pWindowに格納.
+	if (m_mapWindowMap.find(hwnd) != m_mapWindowMap.end()){	// findでキーをhwndとするCWindowオブジェクトポインタが見つかったら.
+		pWindow = m_mapWindowMap[hwnd];	// pWindowにhwndで引けるCWindowオブジェクトポインタを格納.
+	}
+
+	// ウィンドウオブジェクトを取得できない場合.
+	if (pWindow == NULL){	// pWindowがNULLの時.
+
+		// 配列の初期化
+		TCHAR tszClassName[256] = {0};	// tszClassNameを0で初期化.
+
+		// ウィンドウハンドルからウィンドウクラス名を取得.
+		GetClassName(hwnd, tszClassName, 256);	// GetClassNameでウィンドウクラス名を取得.
+
+		// tszClassNameがm_mapBaseWindowProcMapのキーにあれば.
+		if (m_mapBaseWindowProcMap.find(tszClassName) != m_mapBaseWindowProcMap.end()){	// みつかったら.
+
+			// 既定のプロシージャに任せる.
+			return CallWindowProc(m_mapBaseWindowProcMap[tszClassName], hwnd, uMsg, wParam, lParam);	// CallWindowProcで, このメッセージをm_mapBaseWindowProcMap[tszClassName]に任せる.
+
+		}
+		else{	// 無い場合.
+
+			// そうでないなら, DefWindowProcに任せる.
+			return DefWindowProc(hwnd, uMsg, wParam, lParam);	// DefWindowProcの値を返す.
+
+		}
+
+	}
+	else{	// pWindowがあった場合.
+
+		// そのウィンドウのDynamicWindowProcに任せる.
+		return pWindow->DynamicWindowProc(hwnd, uMsg, wParam, lParam);	// pWindow->DynamicWindowProcに渡す.
+
+	}
+
+}
+
+// ウィンドウ作成関数Create.
+BOOL CCustomControl::Create(LPCTSTR lpctszClassName, LPCTSTR lpctszWindowName, DWORD dwStyle, int x, int y, int iWidth, int iHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance){
+
+	// ウィンドウクラス名にはlpctszClassNameを指定.
+	BOOL bRet = CWindow::Create(lpctszClassName, lpctszWindowName, dwStyle | WS_CHILD | WS_VISIBLE, x, y, iWidth, iHeight, hWndParent, hMenu, hInstance);	// CWindow::Createにウィンドウクラス名lpctszClassNameを指定.
+	if (bRet){	// bRetがTRUE.
+
+		// 既定のウィンドウプロシージャを取得し, CCustomControl::StaticWindowProcに差し替える.
+		WNDPROC lpfnWndProc;	// 既定のプロシージャlpfnWndProc
+		lpfnWndProc = (WNDPROC)GetWindowLong(m_hWnd, GWL_WNDPROC);	// GetWindowLongでプロシージャlpfnWndProcを取得.
+		SetWindowLong(m_hWnd, GWL_WNDPROC, (LONG)StaticWindowProc);	// SetWindowLongでプロシージャCCustomControl::StaticWindowProcを設定.
+
+		// OnCreateは以降は呼ばれないので, ここで呼んでおく.
+		CREATESTRUCT cs;	// CREATESTRUCTを一応用意.
+		cs.hInstance = hInstance;	// hInstanceは要るかもしれないので, これは渡せるようにしておく.
+		if (OnCreate(m_hWnd, &cs) != 0){	// OnCreateにm_hWndとcsを渡す.
+
+			// 戻り値が0でない場合, 破棄.
+			Destroy();	// Destroyで破棄.
+			return FALSE;	// FALSEを返す.
+
+		}
+
+		// マップのキーにウィンドウクラス名がなければ登録.
+		if (m_mapBaseWindowProcMap.find(lpctszClassName) == m_mapBaseWindowProcMap.end()){	// マップに無い時.
+			m_mapBaseWindowProcMap.insert(std::pair<LPCTSTR, WNDPROC>(lpctszClassName, lpfnWndProc));	// プロシージャを登録.
+		}
+
+		// WM_CREATEを通らないので, ウィンドウマップの登録も行う.
+		if (m_mapWindowMap.find(m_hWnd) == m_mapWindowMap.end()){	// ウィンドウマップになければ.
+			m_mapWindowMap.insert(std::pair<HWND, CWindow *>(m_hWnd, this));	// 登録する.
+		}
+
+	}
+	return bRet;	// bRetを返す.
+
+}
+
+// ウィンドウ作成関数Create(RECTバージョン).
+BOOL CCustomControl::Create(LPCTSTR lpctszClassName, LPCTSTR lpctszWindowName, DWORD dwStyle, const RECT& rect, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance){
+
+	// RECTで指定されたサイズをx, y, iWidth, iHeightに変換.
+	return CCustomControl::Create(lpctszClassName, lpctszWindowName, dwStyle, rect.left, rect.top, rect.right - rect.left + 1, rect.bottom - rect.top + 1, hWndParent, hMenu, hInstance);	// CCustomControl::Createでカスタムコントロールのウィンドウ作成.
+
+}
+
+// ウィンドウ破棄関数Destroy
+BOOL CCustomControl::Destroy(){
+
+	// 親クラスのDestroyを呼ぶ.
+	return CWindow::Destroy();	// CWindow::Destroyを呼ぶ.
+
+}
+
+// ダイナミックウィンドウプロシージャDynamicWindowProc.
+LRESULT CCustomControl::DynamicWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
+
+	// ウィンドウメッセージの処理
+	switch (uMsg){	// uMsgの値ごとに処理を振り分ける.
+
+		// ウィンドウが破棄された時.
+		case WM_DESTROY:
+
+			// WM_DESTROYブロック
+			{
+
+				// OnDestroyに任せる.
+				OnDestroy();	// OnDestroyを呼ぶ.
+
+			}
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理へ向かう.
+
+		// ウィンドウの描画を要求された時.
+		case WM_PAINT:
+
+			// WM_PAINTブロック
+			{
+
+				// OnPaintに任せる.
+				OnPaint();	// OnPaintを呼ぶ.
+
+			}
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// 垂直スクロールバーがスクロールされた時.
+		case WM_VSCROLL:	// 垂直スクロールバーがスクロールされた時.(uMsgがWM_VSCROLLの時.)
+
+			// WM_VSCROLLブロック
+			{
+
+				// OnVScrollに任せる.
+				OnVScroll(LOWORD(wParam), HIWORD(wParam));	// OnVScrollに任せる.
+
+			}
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// それ以外の時.
+		default:
+
+			// defaultブロック
+			{
+
+			}
+
+ 			// 既定の処理へ向かう.
+ 			break;	// breakで抜けて, 既定の処理へ向かう.
+
+	}
+
+	// 配列の初期化
+	TCHAR tszClassName[256] = {0};	// tszClassNameを0で初期化.
+
+	// ウィンドウハンドルからウィンドウクラス名を取得.
+	GetClassName(hwnd, tszClassName, 256);	// GetClassNameでウィンドウクラス名を取得.
+
+	// tszClassNameがm_mapBaseWindowProcMapのキーにあれば.
+	if (m_mapBaseWindowProcMap.find(tszClassName) != m_mapBaseWindowProcMap.end()){	// みつかったら.
+
+		// 既定のプロシージャに任せる.
+		return CallWindowProc(m_mapBaseWindowProcMap[tszClassName], hwnd, uMsg, wParam, lParam);	// CallWindowProcで, このメッセージをm_mapBaseWindowProcMap[tszClassName]に任せる.
+
+	}
+	else{	// 無い場合.
+
+		// そうでないなら, DefWindowProcに任せる.
+		return DefWindowProc(hwnd, uMsg, wParam, lParam);	// DefWindowProcの値を返す.
+
+	}
+
+}
+
+// ウィンドウの作成が開始された時.
+int CCustomControl::OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct){
+
+	// 常にウィンドウ作成に成功するものとする.
+	return 0;	// 0を返すと, ウィンドウ作成に成功したということになる.
+
+}
+
+// ウィンドウが破棄された時.
+void CCustomControl::OnDestroy(){
+
+	// 自分のウィンドウハンドルが残っていたらマップから削除.
+	if (m_mapWindowMap.find(m_hWnd) != m_mapWindowMap.end()){	// findでみつかったら.
+		m_mapWindowMap.erase(m_hWnd);	// m_mapWindowMap.eraseで削除.
+	}
+
+}
+
+// ウィンドウの描画を要求された時.
+void CCustomControl::OnPaint(){
+
+	// 変数の宣言.
+	HDC hDC;	// デバイスコンテキストハンドルhDC.
+	PAINTSTRUCT ps;	// PAINTSTRUCT構造体ps.
+
+	// 文字列の描画.
+	hDC = BeginPaint(m_hWnd, &ps);	// Win32APIのBeginPaintでhDCを取得.
+	TextOut(hDC, 0, 0, _T("CCustomControl"), (int)_tcslen(_T("CCustomControl")));	// Win32APIのTextOutで"CCustomControl"と描画.
+	EndPaint(m_hWnd, &ps);	// Win32APIのEndPaintで描画終了.
+
+}
+
+// 垂直方向スクロールバーイベント時.
+void CCustomControl::OnVScroll(UINT nSBCode, UINT nPos){
+
+	// 垂直方向スクロールバー情報を取得.
+	SCROLLINFO scrVert = {0};	// 垂直方向スクロール情報scrVertを{0}で初期化.
+	scrVert.cbSize = sizeof(SCROLLINFO);	// sizeofで構造体サイズ指定.
+	scrVert.fMask = SIF_PAGE | SIF_RANGE | SIF_POS;	// ページ, レンジ, 位置を取得.
+	GetScrollInfo(m_hWnd, SB_VERT, &scrVert);	// GetScrollInfoでscrVertを取得.
+
+	// つまみの最大位置を計算.
+	int iMaxPos = scrVert.nMax + 1 - scrVert.nPage;	// セットした最大値 + 1が大きさで, そこからページ数を引くと, つまみの最大の位置.
+
+	// 通知コード処理
+	switch (nSBCode){	// 通知コードが格納されているので, それで判定する.
+
+		// 1番上
+		case SB_TOP:
+
+			// 1番上にセット.
+			scrVert.nPos = scrVert.nMin;	// 現在位置を1番上にセット.
+			break;	// 抜ける.
+
+		// 1番下
+		case SB_BOTTOM:
+
+			// 1番下にセット.
+			scrVert.nPos = scrVert.nMax;	// 現在位置を1番下にセット.
+			break;	// 抜ける.
+
+		// 1列上
+		case SB_LINEUP:
+
+			// 1列上に戻す.
+			if (scrVert.nPos > scrVert.nMin){	// scrVert.nPosがscrVert.nMinより大きい場合.
+				scrVert.nPos--;	// 1戻る.
+			}
+			break;	// 抜ける.
+
+		// 1列下
+		case SB_LINEDOWN:
+
+			// 1列下に進める.
+			if (scrVert.nPos < iMaxPos){	// scrVert.nPosがiMaxPosより小さい場合.
+				scrVert.nPos++;	// 1進む.
+			}
+			break;	// 抜ける.
+
+		// 1ページ上
+		case SB_PAGEUP:
+
+			// SB_PAGEUPブロック.
+			{
+
+				// 1ページ戻る.
+				int after = scrVert.nPos - scrVert.nPage;	// 現在位置から1ページ分引く.
+				if (after >= scrVert.nMin){	// 下限を超えてなければ.
+					scrVert.nPos -= scrVert.nPage;	// 1ページ分マイナス.
+				}
+				else{	// 下限を超えたら.
+					scrVert.nPos = scrVert.nMin;	// 最小値に.
+				}
+
+			}
+
+			// 抜ける.
+			break;	// breakで抜ける.
+
+		// 1ページ下
+		case SB_PAGEDOWN:
+
+			// SB_PAGEDOWNブロック.
+			{
+
+				// 1ページ進む.
+				int after = scrVert.nPos + scrVert.nPage;	// 現在位置から1ページ分足す.
+				if (after <= iMaxPos){	// 上限を超えてなければ.
+					scrVert.nPos += scrVert.nPage;	// 1ページ分プラス.
+				}
+				else{	// 上限を超えたら.
+					scrVert.nPos = scrVert.nMax;	// 最大値に.
+				}
+
+			}
+
+			// 抜ける.
+			break;	// breakで抜ける.
+
+		// スクロールつまみが離された時.
+		case SB_THUMBPOSITION:
+
+
+			// SB_THUMBPOSITIONブロック.
+			{
+
+				// 離された位置をセット.
+				int before = scrVert.nPos;	// 以前.
+				int after = nPos;	// 以後.
+				scrVert.nPos = after;	// HIWORD(wParam)に離された位置が格納されているのでscrVert.nPosにセット.
+
+			}
+
+			// 抜ける.
+			break;	// breakで抜ける.
+
+		// それ以外.
+		default:
+
+			// 抜ける.
+			break;	// breakで抜ける.
+
+	}
+
+	// scrVert.nPosをhwndのSB_VERTにセット.
+	SetScrollInfo(m_hWnd, SB_VERT, &scrVert, TRUE);	// SetScrollInfoで現在のscrVert.nPosをm_hWndにセット.
+	InvalidateRect(m_hWnd, NULL, TRUE);	// InvalidateRectで無効領域を作成.(NULLなので全体が無効領域.)
+	UpdateWindow(m_hWnd);	// UpdateWindowでウィンドウの更新.
+
+	// WM_VSCROLLが発生した時に来たことを示す.
+	HDC hDC = GetDC(m_hWnd);	// hDC取得.
+	static int j = 0;	// スタティック変数jを0で初期化.
+	TCHAR tszJ[16] = {0};	// 文字列バッファtszJ(長さ16)を{0}で初期化.
+	_stprintf(tszJ, _T("j = %d"), j);	// jを文字列tszJに変換.
+	BOOL b = TextOut(hDC, 0, 0, tszJ, _tcslen(tszJ));	// tszJを描画.
+	j++;	// jをインクリメント.
+	ReleaseDC(m_hWnd, hDC);	// hDC解放.
+
+}
---
+++ winapi/CPicture/CPicture_1/src/CPicture/CPicture/MainWindow.cpp
@@ -0,0 +1,243 @@
+// ヘッダのインクルード
+// 既定のヘッダ
+#include <stdio.h>	// C標準入出力
+// 独自のヘッダ
+#include "MainWindow.h"	// CMainWindow
+
+// ウィンドウクラス登録関数RegisterClass.
+BOOL CMainWindow::RegisterClass(HINSTANCE hInstance){
+
+	// ウィンドウクラス名は"CMainWindow".
+	return CWindow::RegisterClass(hInstance, _T("CMainWindow"));	// CWindow::RegisterClassでウィンドウクラス名"CMainWindow"を登録.
+
+}
+
+// ウィンドウクラス登録関数RegisterClass.(メニュー名指定バージョン)
+BOOL CMainWindow::RegisterClass(HINSTANCE hInstance, LPCTSTR lpctszMenuName){
+
+	// メニュー名はlpctszMenuName.
+	return CWindow::RegisterClass(hInstance, _T("CMainWindow"), lpctszMenuName);	// CWindow::RegisterClassで, ウィンドウクラス名"CMainWindow", メニュー名lpctszMenuNameを登録.
+
+}
+
+// コンストラクタCMainWindow()
+CMainWindow::CMainWindow(){
+
+	// メンバの初期化.
+	m_pChild = NULL;	// m_pChildをNULLで初期化.
+	m_pPicture = NULL;	// m_pPictureをNULLで初期化.
+	m_pEdit = NULL;	// m_pEditをNULLで初期化.
+
+}
+
+// デストラクタ~CMainWindow()
+CMainWindow::~CMainWindow(){
+
+	// メンバの終了処理.
+	Destroy();	// Destroyで子ウィンドウの破棄.
+
+}
+
+// ウィンドウ作成関数Create.(ウィンドウクラス名省略バージョン.)
+BOOL CMainWindow::Create(LPCTSTR lpctszWindowName, DWORD dwStyle, int x, int y, int iWidth, int iHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance){
+
+	// ウィンドウクラス名は"CMainWindow".
+	return CWindow::Create(_T("CMainWindow"), lpctszWindowName, dwStyle, x, y, iWidth, iHeight, hWndParent, hMenu, hInstance);	// CWindow::Createにウィンドウクラス名"CMainWindow"を指定.
+
+}
+
+// ウィンドウ破棄関数Destroy
+BOOL CMainWindow::Destroy(){
+
+	// 変数の初期化.
+	BOOL bRet = FALSE;	// bRetをFALSEで初期化.
+
+	// DestroyChildrenを分けたので, 自身のウィンドウ破棄は問題ない.
+	// まず子ウィンドウの破棄.
+	DestroyChildren();
+
+	// 自身のウィンドウ破棄.
+	bRet = CWindow::Destroy();	// 戻り値をbRetに格納.
+
+	// bRetを返す.
+	return bRet;
+
+}
+
+// 子ウィンドウ破棄関数DestroyChildren
+BOOL CMainWindow::DestroyChildren(){
+
+	// 変数の初期化.
+	BOOL bRet = FALSE;	// bRetをFALSEで初期化.
+
+	// 子ウィンドウの破棄.
+	if (m_pChild != NULL) {	// NULLでなければ.
+		bRet = m_pChild->Destroy();	// m_pChild->Destroyでウィンドウを破棄.
+		delete m_pChild;	// deleteでm_pChildを解放.
+		m_pChild = NULL;	// NULLをセット.
+	}
+
+	// ピクチャーの破棄.
+	if (m_pPicture != NULL) {	// NULLでなければ.
+		bRet = m_pPicture->Destroy();	// m_pPicture->Destroyでウィンドウを破棄.
+		delete m_pPicture;	// deleteでm_pPictureを解放.
+		m_pPicture = NULL;	// NULLをセット.
+	}
+
+	// エディットコアの破棄.
+	if (m_pEdit != NULL) {	// NULLでなければ.
+		bRet = m_pEdit->Destroy();	// m_pEdit->Destroyでウィンドウを破棄.
+		delete m_pEdit;	// deleteでm_pEditを解放.
+		m_pEdit = NULL;	// NULLをセット.
+	}
+
+	// 破棄したらTRUEを返す.
+	if (bRet) {	// TRUEなら.
+		return TRUE;	// TRUEを返す.
+	}
+
+	// 破棄しなければ, CWindowのDestroyChildrenを返す.
+	return CWindow::DestroyChildren();	// CWindow::DestroyChildrenを返す.
+
+}
+
+// ウィンドウの作成が開始された時.
+int CMainWindow::OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct){
+
+	// 水平方向スクロールバーの初期化.
+	SCROLLINFO scrHorz = {0};	// 水平方向スクロール情報scrHorzを{0}で初期化.
+	scrHorz.cbSize = sizeof(SCROLLINFO);	// sizeofで構造体サイズ指定.
+	scrHorz.nMin = 0;	// 最小値は0.
+	scrHorz.nMax = 640 - 1;	// 最大値は639.
+	scrHorz.nPage = 100;	// ページサイズは100.
+	scrHorz.nPos = 0;	// 現在位置は0.
+	scrHorz.fMask = SIF_PAGE | SIF_RANGE | SIF_POS;	// ページ, レンジ, 位置をセット.
+	SetScrollInfo(hwnd, SB_HORZ, &scrHorz, TRUE);	// スクロール情報をセット.
+
+	// 垂直方向スクロールバーの初期化.
+	SCROLLINFO scrVert = {0};	// 垂直方向スクロール情報scrVertを{0}で初期化.
+	scrVert.cbSize = sizeof(SCROLLINFO);	// sizeofで構造体サイズ指定.
+	scrVert.nMin = 0;	// 最小値は0.
+	scrVert.nMax = 480 - 1;	// 最大値は479.
+	scrVert.nPage = 100;	// ページサイズは100.
+	scrVert.nPos = 0;	// 現在位置は0.
+	scrVert.fMask = SIF_PAGE | SIF_RANGE | SIF_POS;	// ページ, レンジ, 位置をセット.
+	SetScrollInfo(hwnd, SB_VERT, &scrVert, TRUE);	// スクロール情報をセット.
+
+	// 子ウィンドウの作成.
+	m_pChild = new CWindow();	// CWindowオブジェクトm_pChild作成.
+	RECT rc;	// RECT構造体rc.
+	rc.left = 50;		// 左50
+	rc.right = 150;		// 右150
+	rc.top = 250;		// 上250
+	rc.bottom = 350;	// 下350
+	m_pChild->Create(_T("Child"), _T(""), WS_BORDER | WS_CHILD | WS_VISIBLE | WS_VSCROLL, rc, hwnd, (HMENU)(WM_APP + 1), lpCreateStruct->hInstance);	// Createでウィンドウクラス"Child"のウィンドウ作成.	
+
+	// 子ウィンドウの垂直方向スクロールバーの初期化.
+	SCROLLINFO scrVertChild = {0};	// 垂直方向スクロール情報scrVertChildを{0}で初期化.
+	scrVertChild.cbSize = sizeof(SCROLLINFO);	// sizeofで構造体サイズ指定.
+	scrVertChild.nMin = 0;	// 最小値は0.
+	scrVertChild.nMax = 480 - 1;	// 最大値は479.
+	scrVertChild.nPage = 100;	// ページサイズは100.
+	scrVertChild.nPos = 0;	// 現在位置は0.
+	scrVertChild.fMask = SIF_PAGE | SIF_RANGE | SIF_POS;	// ページ, レンジ, 位置をセット.
+	SetScrollInfo(m_pChild->m_hWnd, SB_VERT, &scrVertChild, TRUE);	// スクロール情報をセット.
+
+	// ピクチャーコントロールのウィンドウ作成.
+	m_pPicture = new CPicture();	// newでCPictureオブジェクトを作成し, ポインタm_pPictureに格納.
+	/*
+	RECT rc2;	// RECT構造体rc2.
+	rc2.left = 150;		// 左150
+	rc2.right = 250;		// 右250
+	rc2.top = 250;		// 上250
+	rc2.bottom = 350;	// 下350
+	*/
+	//m_pPicture->Create(_T(""), WS_BORDER | WS_VSCROLL | SS_BITMAP, rc2, hwnd, (HMENU)(WM_APP + 2), lpCreateStruct->hInstance);	// Createでピクチャーコントロールのウィンドウ作成.
+	m_pPicture->Create(_T("CPicutre"), WS_BORDER | WS_VSCROLL | SS_BITMAP, 150, 250, 100, 100, hwnd, (HMENU)(WM_APP + 2), lpCreateStruct->hInstance);	// Createでピクチャーコントロールのウィンドウ作成.
+
+	// ピクチャーの垂直方向スクロールバーの初期化.
+	SCROLLINFO scrVertPicture = {0};	// 垂直方向スクロール情報scrVertPictureを{0}で初期化.
+	scrVertPicture.cbSize = sizeof(SCROLLINFO);	// sizeofで構造体サイズ指定.
+	scrVertPicture.nMin = 0;	// 最小値は0.
+	scrVertPicture.nMax = 480 - 1;	// 最大値は479.
+	scrVertPicture.nPage = 100;	// ページサイズは100.
+	scrVertPicture.nPos = 0;	// 現在位置は0.
+	scrVertPicture.fMask = SIF_PAGE | SIF_RANGE | SIF_POS;	// ページ, レンジ, 位置をセット.
+	SetScrollInfo(m_pPicture->m_hWnd, SB_VERT, &scrVertPicture, TRUE);	// スクロール情報をセット.
+
+	// エディットコアコントロールのウィンドウ作成.
+	m_pEdit = new CEditCore();	// newでCEditCoreオブジェクトを作成し, ポインタをm_pEditに格納.
+	RECT rc3;	// RECT構造体rc3.
+	rc3.left = 250;		// 左250
+	rc3.right = 350;		// 右350
+	rc3.top = 250;		// 上250
+	rc3.bottom = 350;	// 下350
+	m_pEdit->Create(_T("Edit1"), WS_BORDER | WS_VSCROLL, rc3, hwnd, (HMENU)(WM_APP + 3), lpCreateStruct->hInstance);	// Createでエディットコアコントロール"Edit1"のウィンドウ作成.
+
+	// エディットコアの垂直方向スクロールバーの初期化.
+	SCROLLINFO scrVertEdit = {0};	// 垂直方向スクロール情報scrVertEditを{0}で初期化.
+	scrVertEdit.cbSize = sizeof(SCROLLINFO);	// sizeofで構造体サイズ指定.
+	scrVertEdit.nMin = 0;	// 最小値は0.
+	scrVertEdit.nMax = 480 - 1;	// 最大値は479.
+	scrVertEdit.nPage = 100;	// ページサイズは100.
+	scrVertEdit.nPos = 0;	// 現在位置は0.
+	scrVertEdit.fMask = SIF_PAGE | SIF_RANGE | SIF_POS;	// ページ, レンジ, 位置をセット.
+	SetScrollInfo(m_pEdit->m_hWnd, SB_VERT, &scrVertEdit, TRUE);	// スクロール情報をセット.
+	
+	// 親クラスのOnCreateを呼ぶ.
+	return CWindow::OnCreate(hwnd, lpCreateStruct);	// CWindow::OnCreateを呼び, 戻り値を返す.
+
+}
+
+// ウィンドウが破棄された時.
+void CMainWindow::OnDestroy(){
+
+	// CWindowのOnDestroyを呼ぶ.
+	CWindow::OnDestroy();	// CWindow::OnDestroyを呼ぶ.
+
+}
+
+// ウィンドウの描画を要求された時.
+void CMainWindow::OnPaint(){
+
+	// 変数の宣言.
+	HDC hDC;	// デバイスコンテキストハンドルhDC.
+	PAINTSTRUCT ps;	// PAINTSTRUCT構造体ps.
+
+	// 水平方向スクロールバー情報を取得.
+	SCROLLINFO scrHorz = {0};	// 水平方向スクロール情報scrHorzを{0}で初期化.
+	scrHorz.cbSize = sizeof(SCROLLINFO);	// sizeofで構造体サイズ指定.
+	scrHorz.fMask = SIF_POS;	// 位置を取得.
+	GetScrollInfo(m_hWnd, SB_HORZ, &scrHorz);	// GetScrollInfoでscrHorzを取得.
+
+	// 垂直方向スクロールバー情報を取得.
+	SCROLLINFO scrVert = {0};	// 垂直方向スクロール情報scrVertを{0}で初期化.
+	scrVert.cbSize = sizeof(SCROLLINFO);	// sizeofで構造体サイズ指定.
+	scrVert.fMask = SIF_POS;	// 位置を取得.
+	GetScrollInfo(m_hWnd, SB_VERT, &scrVert);	// GetScrollInfoでscrVertを取得.
+
+	// 水平方向と垂直方向のスクロールバーの位置の描画.
+	hDC = BeginPaint(m_hWnd, &ps);	// Win32APIのBeginPaintでhDCを取得.
+	TCHAR tszPos[128] = {0};	// scrHorz.nPosとscrVert.nPosを文字列にしたものtszPos(長さ128)を{0}で初期化.
+	_stprintf(tszPos, _T("scrHorz.nPos = %d, scrVert.nPos = %d"), scrHorz.nPos, scrVert.nPos);	// scrHorz.nPosとscrVert.nPosを文字列tszPosに変換.
+	TextOut(hDC, 100, 100, tszPos, (int)_tcslen(tszPos));	// TextOutでtszPosを描画.
+	EndPaint(m_hWnd, &ps);	// Win32APIのEndPaintで描画終了.
+
+}
+
+// ウィンドウが閉じられる時.
+int CMainWindow::OnClose(){
+
+	// メッセージボックスで"Close CMainWindow OK?"と表示.
+	int iRet = MessageBox(m_hWnd, _T("Close CMainWindow OK?"), _T("CPicture"), MB_OKCANCEL);	// MessageBoxで"Close CMainWindow OK?"と表示し, 戻り値をiRetに格納.
+	if (iRet != IDOK){	// OK以外.(Cancelなど.)
+		return -1;	// -1を返す.
+	}
+
+	// このウィンドウの破棄.(OnCloseの後, ウィンドウの破棄処理が勝手に行われるので, Destroyは不要なのでコメントアウト.)
+	//Destroy();	// Destroyでこのウィンドウの破棄処理.
+
+	// OKなので閉じる.
+	return CWindow::OnClose();	// 親クラスのOnCloseを呼ぶ.(親クラスのOnCloseは常に閉じる処理になっている.)
+
+}
