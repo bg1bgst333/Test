adab8d966e86c7045130ca1c8c34bc3cd8d75d34
bg1bgst333 <bg1bgst333@gmail.com>
2023-10-21 13:49:58 +0900

    add print_file_text_cstdio#call_from_cpp_Windows

---
+++ cpp/print_file_text_cstdio/call_from_cpp_Windows/src/print_file_text_cstdio/print_file_text_cstdio/file_utility_cstdio.c
@@ -0,0 +1,30 @@
+/* ヘッダファイルのインクルード */
+/* 独自のヘッダファイル */
+#include "file_utility_cstdio.h"	/* ファイルユーティリティ(C標準入出力) */
+
+/* C標準入出力ライブラリ関数でテキストファイル出力. */
+int print_file_text_cstdio(const char* path, const char* text) {
+
+	/* 変数・構造体・ポインタの初期化 */
+	FILE* fp = NULL;	/* ファイルポインタをNULLで初期化. */
+	int ret = 0;	/* 書き込んだバイト数retを0で初期化. */
+
+	/* ファイルを開く. */
+	fp = fopen(path, "w");	/* fopenでテキスト書き込みで開く. */
+	if (fp != NULL) {	/* fpがNULLでない時. */
+
+		/* ファイルにテキストを書き込む. */
+		ret = fprintf(fp, "%s", text);	/* fprintfでfpにtextを出力し, 戻り値はretに格納. */
+
+		/* fpを閉じる. */
+		fclose(fp);	/* fcloseでfpを閉じる. */
+
+		/* 書き込んだバイト数retを返す. */
+		return ret;	/* returnでretを返す. */
+
+	}
+
+	/* ファイルを開けない場合は, -1を返す. */
+	return -1;	/* returnで-1を返す. */
+
+}
---
+++ cpp/print_file_text_cstdio/call_from_cpp_Windows/src/print_file_text_cstdio/print_file_text_cstdio/file_utility_cstdio.h
@@ -0,0 +1,13 @@
+/* 二重インクルード防止 */
+#ifndef __FILE_UTILITY_CSTDIO_H__
+#define __FILE_UTILITY_CSTDIO_H__
+
+/* ヘッダファイルのインクルード */
+/* 既定のヘッダファイル */
+#include <stdio.h>	/* 標準入出力 */
+#include <string.h>	/* 文字列処理 */
+
+/* 関数のプロトタイプ宣言 */
+int print_file_text_cstdio(const char* path, const char* text);	/* 関数print_file_text_cstdioの宣言 */
+
+#endif
