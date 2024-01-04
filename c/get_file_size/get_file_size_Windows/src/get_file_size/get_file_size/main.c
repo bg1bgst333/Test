f800cbaeed81f0f6e80116a109c6db3a3a87e5e7
bg1bgst333 <bg1bgst333@gmail.com>
2024-01-04 17:04:46 +0900

    add get_file_size#get_file_size_Windows

---
+++ c/get_file_size/get_file_size_Windows/src/get_file_size/get_file_size/main.c
@@ -0,0 +1,35 @@
+/* ヘッダファイルのインクルード */
+#include <stdio.h>		/* 標準入出力 */
+#include <sys/types.h>	/* 派生型 */
+#include <sys/stat.h>	/* ファイル状態 */
+
+/* 関数のプロトタイプ宣言 */
+size_t get_file_size(const char* path);	/* 関数get_file_sizeの宣言 */
+
+/* main関数の定義 */
+int main(void) {
+
+	/* ファイルサイズの取得. */
+	size_t file_size = get_file_size("test.txt");	/* get_file_sizeで"test.txt"のファイルサイズを取得. */
+
+	/* file_sizeを出力. */
+	printf("file_size = %d\n", file_size);	/* printfでfile_sizeを取得. */
+
+	/* プログラムの終了 */
+	return 0;	/* 0を返して正常終了. */
+
+}
+
+/* ファイルサイズの取得 */
+size_t get_file_size(const char* path) {
+
+	/* 構造体の初期化. */
+	struct _stat sst = { 0 };	/* _stat構造体sstを{0}で初期化. */
+
+	/* ファイル情報の取得. */
+	_stat(path, &sst);	/* _statでpathで示されたファイルの情報をsstに格納. */
+
+	/* ファイルサイズを返す. */
+	return sst.st_size;	/* returnでsst.st_sizeを返す. */
+
+}
