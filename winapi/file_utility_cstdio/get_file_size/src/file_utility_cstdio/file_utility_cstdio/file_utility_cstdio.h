fccf204412007a26715de1a39c78c0223fa25783
bg1bgst333 <bg1bgst333@gmail.com>
2024-01-12 18:01:49 +0900

    add file_utility_cstdio#get_file_size

---
+++ winapi/file_utility_cstdio/get_file_size/src/file_utility_cstdio/file_utility_cstdio/file_utility_cstdio.h
@@ -0,0 +1,15 @@
+/* 二重インクルード防止 */
+#ifndef __FILE_UTILITY_CSTDIO_H__
+#define __FILE_UTILITY_CSTDIO_H__
+
+/* ヘッダファイルのインクルード */
+/* 既定のヘッダファイル */
+#include <stdio.h>	/* 標準入出力 */
+#include <string.h>	/* 文字列処理 */
+#include <sys/stat.h>	/* ファイル状態 */
+
+/* 関数のプロトタイプ宣言 */
+int print_file_text_cstdio(const char *path, const char *text);	/* 関数print_file_text_cstdioの宣言 */
+size_t get_file_size(const char *path);	/* 関数get_file_sizeの宣言. */
+
+#endif
