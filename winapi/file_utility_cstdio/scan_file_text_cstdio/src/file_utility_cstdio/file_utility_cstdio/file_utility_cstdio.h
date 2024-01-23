50b735300986233f31a62e999730b686f90e338d
bg1bgst333 <bg1bgst333@gmail.com>
2024-01-23 18:29:05 +0900

    add file_utility_cstdio#scan_file_text_cstdio

---
+++ winapi/file_utility_cstdio/scan_file_text_cstdio/src/file_utility_cstdio/file_utility_cstdio/file_utility_cstdio.h
@@ -0,0 +1,16 @@
+/* 二重インクルード防止 */
+#ifndef __FILE_UTILITY_CSTDIO_H__
+#define __FILE_UTILITY_CSTDIO_H__
+
+/* ヘッダファイルのインクルード */
+/* 既定のヘッダファイル */
+#include <stdio.h>	/* 標準入出力 */
+#include <string.h>	/* 文字列処理 */
+#include <sys/stat.h>	/* ファイル状態 */
+
+/* 関数のプロトタイプ宣言 */
+int print_file_text_cstdio(const char* path, const char* text);	/* 関数print_file_text_cstdioの宣言 */
+size_t get_file_size(const char* path);	/* 関数get_file_sizeの宣言. */
+int scan_file_text_cstdio(const char* path, char* text);	/* 関数scan_file_text_cstdioの宣言. */
+
+#endif
