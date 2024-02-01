34564adec09750df1b58bb4dbddf0ab5b103dee7
bg1bgst333 <bg1bgst333@gmail.com>
2024-02-01 20:39:28 +0900

    add get_file_text_cstdio#call_from_cpp_Windows

---
+++ cpp/get_file_text_cstdio/call_from_cpp_Windows/src/get_file_text_cstdio/get_file_text_cstdio/file_utility_cstdio.c
@@ -0,0 +1,44 @@
+/* ヘッダファイルのインクルード */
+/* 独自のヘッダファイル */
+#include "file_utility_cstdio.h"	/* ファイルユーティリティ(C標準入出力) */
+
+/* ファイルサイズの取得. */
+size_t get_file_size(const char* path) {
+
+	/* 構造体の初期化. */
+	struct _stat sst = { 0 };	/* _stat構造体sstを{0}で初期化. */
+
+	/* ファイル情報の取得. */
+	_stat(path, &sst);	/* _statでpathで示されたファイルの情報をsstに格納. */
+
+	/* ファイルサイズを返す. */
+	return sst.st_size;	/* returnでsst.st_sizeを返す. */
+
+}
+
+/* C標準入出力ライブラリ関数でテキストファイル入力. */
+char* get_file_text_cstdio(const char* path, char* text, size_t buf_size) {
+
+	/* 構造体・ポインタの宣言・初期化. */
+	FILE* fp = NULL;	/* ファイルポインタfpをNULLで初期化. */
+	char* ret;	/* 格納した文字列ポインタret. */
+
+	/* ファイルを開く. */
+	fp = fopen(path, "r");	/* fopenでテキスト読み込みで開く. */
+	if (fp != NULL) {	/* fpがNULLでない時. */
+
+		/* ファイルからテキストを読み込む. */
+		ret = fgets(text, buf_size, fp);	/* fgetsでfpから入力しtextに格納, 戻り値はretに格納. */
+
+		/* fpを閉じる. */
+		fclose(fp);	/* fcloseでfpを閉じる. */
+
+		/* retを返す. */
+		return ret;	/* returnでretを返す. */
+
+	}
+
+	/* ファイルを開けない場合は, NULLを返す. */
+	return NULL;	/* returnでNULLを返す. */
+
+}
---
+++ cpp/get_file_text_cstdio/call_from_cpp_Windows/src/get_file_text_cstdio/get_file_text_cstdio/file_utility_cstdio.h
@@ -0,0 +1,14 @@
+/* 二重インクルード防止 */
+#ifndef __FILE_UTILITY_CSTDIO_H__
+#define __FILE_UTILITY_CSTDIO_H__
+
+/* ヘッダファイルのインクルード */
+/* 既定のヘッダファイル */
+#include <stdio.h>	/* 標準入出力 */
+#include <sys/stat.h>	/* ファイル状態 */
+
+/* 関数のプロトタイプ宣言 */
+size_t get_file_size(const char* path);	/* 関数get_file_sizeの宣言. */
+char* get_file_text_cstdio(const char* path, char* text, size_t buf_size);	/* 関数get_file_text_cstdioの宣言. */
+
+#endif
