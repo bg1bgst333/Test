6cac28fec0164514c4988f4d29feda35655d851b
bg1bgst333 <bg1bgst333@gmail.com>
2024-02-15 12:15:28 +0900

    add read_file_binary_cstdio#call_from_cpp_Windows

---
+++ cpp/read_file_binary_cstdio/call_from_cpp_Windows/src/read_file_binary_cstdio/read_file_binary_cstdio/file_utility_cstdio.h
@@ -0,0 +1,14 @@
+/* 二重インクルード防止 */
+#ifndef __FILE_UTILITY_CSTDIO_H__
+#define __FILE_UTILITY_CSTDIO_H__
+
+/* ヘッダファイルのインクルード */
+/* 既定のヘッダファイル */
+#include <stdio.h>	/* 標準入出力 */
+#include <sys/stat.h>	/* ファイル状態 */
+
+/* 関数のプロトタイプ宣言 */
+size_t get_file_size(const char* path);	/* 関数get_file_sizeの宣言. */
+size_t read_file_binary_cstdio(const char* path, void* bytes, size_t size);	/* 関数read_file_binary_cstdioの宣言. */
+
+#endif
