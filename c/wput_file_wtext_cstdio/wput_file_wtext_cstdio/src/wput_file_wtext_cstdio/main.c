a4e2bb7d94a18013cf703ea657897b3f84bac275
B.G <bg1bgst333@gmail.com>
2024-02-22 13:10:08 +0900

    add wput_file_wtext_cstdio

--- c/wput_file_wtext_cstdio/wput_file_wtext_cstdio/src/wput_file_wtext_cstdio/main.c
+++ c/wput_file_wtext_cstdio/wput_file_wtext_cstdio/src/wput_file_wtext_cstdio/main.c
@@ -1,38 +1,39 @@
 /* ヘッダファイルのインクルード */
 #include <stdio.h> /* 標準入出力 */
 #include <locale.h> /* ロケール */
+#include <wchar.h> /* ワイド文字 */
 
 /* 関数のプロトタイプ宣言 */
-int put_file_text_cstdio(const char *path, const char *text); /* 関数put_file_text_cstdioの宣言. */
+int wput_file_wtext_cstdio(const char *path, const wchar_t *wtext); /* 関数wput_file_wtext_cstdioの宣言. */
 
 /* main関数の定義 */
 int main(void){
 
   /* 配列の初期化. */
-  char japanese_str[] = "あいうえお\nかきくけこ\nさしすせそ"; /* '\n'が含まれた日本語文字列. */
+  wchar_t japanese_wstr[] = L"あいうえお\nかきくけこ\nさしすせそ"; /* '\n'が含まれた日本語文字列. */
 
-  /* japanese_strを"test1.txt"に出力. */
-  put_file_text_cstdio("test1.txt", japanese_str); /* put_file_text_cstdioでjapanese_strを"test1.txt"に出力. */
+  /* japanese_wstrを"test1.txt"に出力. */
+  wput_file_wtext_cstdio("test1.txt", japanese_wstr); /* wput_file_wtext_cstdioでjapanese_wstrを"test1.txt"に出力. */
 
   /* ロケール"ja_JP.UTF-8"のセット. */
   setlocale(LC_ALL, "ja_JP.UTF-8"); /* setlocaleでLC_ALL, "ja_JP.UTF-8"をセット. */
 
-  /* japanese_strを"test2.txt"に出力. */
-  put_file_text_cstdio("test2.txt", japanese_str); /* put_file_text_cstdioでjapanese_strを"test2.txt"に出力. */
+  /* japanese_wstrを"test2.txt"に出力. */
+  wput_file_wtext_cstdio("test2.txt", japanese_wstr); /* wput_file_wtext_cstdioでjapanese_wstrを"test2.txt"に出力. */
 
   /* ロケール"ja_JP.EUC-JP"のセット. */
   setlocale(LC_ALL, "ja_JP.EUC-JP"); /* setlocaleでLC_ALL, "ja_JP.EUC-JP"をセット. */
 
-  /* japanese_strを"test3.txt"に出力. */
-  put_file_text_cstdio("test3.txt", japanese_str); /* put_file_text_cstdioでjapanese_strを"test3.txt"に出力. */
+  /* japanese_wstrを"test3.txt"に出力. */
+  wput_file_wtext_cstdio("test3.txt", japanese_wstr); /* wput_file_wtext_cstdioでjapanese_wstrを"test3.txt"に出力. */
 
   /* プログラムの終了 */
   return 0; /* 0を返して正常終了. */
 
 }
 
-/* C標準入出力ライブラリ関数で文字テキストファイル出力. */
-int put_file_text_cstdio(const char *path, const char *text){
+/* C標準入出力ライブラリ関数でワイド文字テキストファイル出力. */
+int wput_file_wtext_cstdio(const char *path, const wchar_t *wtext){
 
   /* 変数・構造体の初期化. */
   FILE *fp = NULL; /* ファイルポインタfpをNULLで初期化. */
@@ -42,8 +43,8 @@ int put_file_text_cstdio(const char *path, const char *text){
   fp = fopen(path, "w"); /* fopenでテキスト書き込みで開く. */
   if (fp != NULL){ /* fpがNULLでない時. */
 
-    /* ファイルにテキストを書き込む. */
-    ret = fputs(text, fp); /* fputsでfpにtextを出力し, 戻り値はretに格納. */
+    /* ファイルにワイド文字テキストを書き込む. */
+    ret = fputws(wtext, fp); /* fputwsでfpにwtextを出力し, 戻り値はretに格納. */
 
     /* fpを閉じる. */
     fclose(fp); /* fcloseでfpを閉じる. */
