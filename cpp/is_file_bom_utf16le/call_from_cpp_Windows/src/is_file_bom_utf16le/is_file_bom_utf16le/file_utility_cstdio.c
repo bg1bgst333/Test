6ba6df5acdfb42f68e7cbf263d4423524ac27186
bg1bgst333 <bg1bgst333@gmail.com>
2024-04-15 10:36:11 +0900

    add is_file_bom_utf16le#call_from_cpp_Windows

---
+++ cpp/is_file_bom_utf16le/call_from_cpp_Windows/src/is_file_bom_utf16le/is_file_bom_utf16le/file_utility_cstdio.c
@@ -0,0 +1,36 @@
+/* ヘッダファイルのインクルード */
+/* 独自のヘッダファイル */
+#include "file_utility_cstdio.h"	/* ファイルユーティリティ(C標準入出力) */
+
+/* UTF-16LEのBOMがあるか判定する. */
+int is_file_bom_utf16le(const char *path){
+
+	/* 変数・構造体の宣言・初期化. */
+	FILE *fp = NULL;	/* ファイルポインタfpをNULLで初期化. */
+	size_t ret;	/* 読み込んだバイト数ret. */
+	char bom[2] = {0};	/* char型配列bom(長さ2)を{0}で初期化. */
+
+	/* ファイルを開く. */
+	fp = fopen(path, "rb");	/* fopenでバイナリ読み込みで開く. */
+	if (fp != NULL){	/* fpがNULLでない時. */
+
+		/* ファイルからバイナリを読み込む. */
+		ret = fread(bom, sizeof(char), 2, fp);	/* freadでfpから入力しbomに格納, 戻り値はretに格納. */
+
+		/* fpを閉じる. */
+		fclose(fp);	/* fcloseでfpを閉じる. */
+
+		/* BOMがUTF-16LEか判定. */
+		if (bom[0] == (char)0xff && bom[1] == (char)0xfe){	/* 0番目が0xff, 1番目が0xfeなら. */
+			return 1;	/* 1を返す. */
+		}
+		else{	/* それ以外. */
+			return 0;	/* 0を返す. */
+		}
+
+	}
+
+	/* ファイルを開けない場合は, 0を返す. */
+	return 0;	/* returnで0を返す. */
+
+}
