bb66ec8eb297ae455ff42c12108000c489e6c9b2
bg1bgst333 <bg1bgst333@gmail.com>
2024-04-23 10:11:30 +0900

    add write_wtext_file_binary_utf16be_cstdio#Windows

---
+++ cpp/write_wtext_file_binary_utf16be_cstdio/Windows/src/w_w_f_b_u_c/w_w_f_b_u_c/main.cpp
@@ -0,0 +1,14 @@
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "cpp_file_utility_cstdio.h"	// C++ファイルユーティリティ(C標準入出力)
+
+// main関数の定義
+int main(){
+
+	// "test.txt"に"あいうえお\r\nかきくけこ\r\nさしすせそ"をワイド文字テキスト(UTF-16BE)としてファイル出力.
+	write_wtext_file_binary_utf16be_cstdio("test.txt", L"あいうえお\r\nかきくけこ\r\nさしすせそ");	// write_wtext_file_binary_utf16be_cstdioでファイル出力.
+
+	// プログラムの終了
+	return 0;	// 0を返して正常終了.
+
+}
