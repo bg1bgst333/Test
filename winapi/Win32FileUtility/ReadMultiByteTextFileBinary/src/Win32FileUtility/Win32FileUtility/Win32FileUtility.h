bbcbb75f9fcc93330f72db12833999f5c9a875e5
bg1bgst333 <bg1bgst333@gmail.com>
2024-05-08 08:32:04 +0900

    add Win32FileUtility#ReadMultiByteTextFileBinary

---
+++ winapi/Win32FileUtility/ReadMultiByteTextFileBinary/src/Win32FileUtility/Win32FileUtility/Win32FileUtility.h
@@ -0,0 +1,13 @@
+// 二重インクルード防止
+#ifndef __WIN32_FILE_UTILITY_H__
+#define __WIN32_FILE_UTILITY_H__
+
+// ヘッダファイルのインクルード
+// 既定のヘッダファイル
+#include <windows.h>	// 標準WindowsAPI
+#include <string>	// std::string
+
+// 関数のプロトタイプ宣言
+BOOL ReadMultiByteTextFileBinary(LPCSTR lpcszFileName, std::wstring& wstrWideText, UINT uiCodePage, LPDWORD lpdwLen);	// 関数ReadMultiByteTextFileBinaryの宣言.
+
+#endif
