b10c2742791d6907a2b2a1ea7a88d86a74984385
bg1bgst333 <bg1bgst333@gmail.com>
2024-06-09 23:58:45 +0900

    add CMenu

---
+++ winapi/CMenu/CMenu/src/CMenu/CMenu/Menu.cpp
@@ -0,0 +1,11 @@
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "Menu.h"	// CMenu
+
+// コンストラクタCMenu
+CMenu::CMenu(){
+
+	// メンバの初期化
+	m_hMenu = NULL;	// m_hMenuをNULLで初期化.
+
+}
---
+++ winapi/CMenu/CMenu/src/CMenu/CMenu/resource.h
@@ -0,0 +1,3 @@
+#define IDR_MAINMENU	100
+#define ID_ITEM_1_1	10101
+#define ID_ITEM_1_2	10102
