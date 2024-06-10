ee88a6da247fc1aca106e12b870d175d9fdbbc60
bg1bgst333 <bg1bgst333@gmail.com>
2024-06-10 16:37:02 +0900

    add CMenu

---
+++ winapi/CMenu/CMenu/src/CMenu/CMenu/Menu.h
@@ -0,0 +1,24 @@
+// 二重インクルード防止
+#ifndef __MENU_H__
+#define __MENU_H__
+
+// ヘッダのインクルード
+// 既定のヘッダ
+#include <windows.h>	// 標準WindowsAPI
+
+// メニュークラスCMenuの定義
+class CMenu {
+
+	// publicメンバ
+	public:
+
+		// publicメンバ変数
+		HMENU m_hMenu;	// メニューハンドルm_hMenu
+
+		// publicメンバ関数
+		// コンストラクタ
+		CMenu();	// コンストラクタCMenu
+
+};
+
+#endif
