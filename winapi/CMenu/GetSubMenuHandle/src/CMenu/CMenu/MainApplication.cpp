713ed548444a1d016d2962da6d6fba444c841c71
bg1bgst333 <bg1bgst333@gmail.com>
2024-06-11 12:50:46 +0900

    add CMenu::GetSubMenuHandle

---
+++ winapi/CMenu/GetSubMenuHandle/src/CMenu/CMenu/MainApplication.cpp
@@ -0,0 +1,31 @@
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "resource.h"
+#include "MainApplication.h"	// CMainApplication
+#include "MainWindow.h"			// CMainWindow
+
+// インスタンス初期化関数InitInstance.
+BOOL CMainApplication::InitInstance(HINSTANCE hInstance, LPTSTR lpCmdLine, int nShowCmd) {
+
+	// ウィンドウクラスの登録.
+	CMainWindow::RegisterClass(hInstance);
+
+	// CMainWindowオブジェクトの作成.
+	m_pMainWnd = new CMainWindow();	// CMainWindowオブジェクトを作成し, m_pMainWndに格納.
+
+	// ウィンドウの作成.
+	HMENU hMenu = LoadMenu(hInstance, MAKEINTRESOURCE(IDR_MAINMENU));
+	if (!m_pMainWnd->Create(_T("CMenu"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, hMenu, hInstance)) {	// m_pMainWnd->Createでウィンドウ作成し, 失敗した場合.
+
+		// エラー処理
+		return FALSE;	// returnでFALSEを返して異常終了.
+
+	}
+
+	// ウィンドウの表示.
+	m_pMainWnd->ShowWindow(SW_SHOW);	// m_pMainWnd->ShowWindowで表示.
+
+	// TRUEを返す.
+	return TRUE;	// returnでTRUEを返す.
+
+}
