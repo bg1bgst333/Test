836f074995a5e7ec1c6a9e062996ecf29bd4e199
B.G <bg1bgst333@gmail.com>
2024-07-07 12:12:09 +0900

    add replace_byte_all#call_from_cpp

---
+++ cpp/replace_byte_all/call_from_cpp/src/replace_byte_all/string_utility_cstring.c
@@ -0,0 +1,23 @@
+/* ヘッダファイルのインクルード */
+/* 独自のヘッダファイル */
+#include "string_utility_cstring.h" /* 文字列ユーティリティ(C文字列処理) */
+
+/* 長さsizeのバイト列bytesのみつかった全てのbeforeをafterに置換. */
+int replace_byte_all(char *bytes, int size, char before, char after){
+
+  /* 変数の宣言・初期化. */
+  int i;
+  int total = 0;
+
+  /* 最後まで探す. */
+  for (i = 0; i < size; i++){
+    if (*(bytes + i) == before){
+      *(bytes + i) = after; /* afterに置換. */
+      total++;
+    }
+  }
+
+  /* みつかった数を返す. */
+  return total;
+
+}
