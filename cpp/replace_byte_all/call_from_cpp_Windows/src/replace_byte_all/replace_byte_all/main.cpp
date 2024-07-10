57135dfc1fec0286223715ed817fa92bbb86d550
bg1bgst333 <bg1bgst333@gmail.com>
2024-07-10 10:33:46 +0900

    add replace_byte_all#call_from_cpp_Windows

---
+++ cpp/replace_byte_all/call_from_cpp_Windows/src/replace_byte_all/replace_byte_all/main.cpp
@@ -0,0 +1,22 @@
+// ヘッダファイルのインクルード
+extern "C"{	// C言語として解釈する.
+	#include "string_utility_cstring.h"	// 文字列ユーティリティ(C文字列処理)
+}
+
+// main関数の定義
+int main(){
+
+	// 配列の初期化.
+	char bytes[] = {0xa, 0xd, 0, 0xd, 0xd};
+
+	// みつかった全てを置換.
+	int ret = replace_byte_all(bytes, 5, 0xd, 0);
+	for (int i = 0;  i < 5; i++){
+		printf("bytes[%d] = 0x%x\n", i, bytes[i]);
+	}
+	printf("ret = %d\n", ret);
+
+	// プログラムの終了.
+	return 0;
+
+}
---
+++ cpp/replace_byte_all/call_from_cpp_Windows/src/replace_byte_all/replace_byte_all/string_utility_cstring.h
@@ -0,0 +1,13 @@
+/* 二重インクルード防止 */
+#ifndef __STRING_UTILITY_CSTRING_H__
+#define __STRING_UTILITY_CSTRING_H__
+
+/* ヘッダファイルのインクルード */
+/* 既定のヘッダファイル */
+#include <stdio.h>	/* 標準入出力 */
+#include <string.h>	/* 文字列処理 */
+
+/* 関数のプロトタイプ宣言 */
+int replace_byte_all(char *bytes, int size, char before, char after);	/* 関数replace_byte_allの宣言. */
+
+#endif
