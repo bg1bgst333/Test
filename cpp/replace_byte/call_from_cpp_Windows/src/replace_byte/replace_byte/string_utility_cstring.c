da8dd01a6610fb7e3c58b3ddcd304f7fefd6d5fa
bg1bgst333 <bg1bgst333@gmail.com>
2024-07-11 12:25:11 +0900

    add replace_byte#call_from_cpp_Windows

---
+++ cpp/replace_byte/call_from_cpp_Windows/src/replace_byte/replace_byte/string_utility_cstring.c
@@ -0,0 +1,22 @@
+/* ヘッダファイルのインクルード */
+/* 独自のヘッダファイル */
+#include "string_utility_cstring.h"	/* 文字列ユーティリティ(C文字列処理) */
+
+/* 長さsizeのバイト列bytesの最初にみつかったbeforeをafterに置換. */
+int replace_byte(char* bytes, int size, char before, char after) {
+
+	/* 変数の宣言 */
+	int i;
+
+	/* beforeがみつかるまで探す. */
+	for (i = 0; i < size; i++) {
+		if (*(bytes + i) == before) {
+			*(bytes + i) = after;	/* afterに置換. */
+			return i;	/* iを返す. */
+		}
+	}
+
+	/* みつからない場合は-1. */
+	return -1;
+
+}
