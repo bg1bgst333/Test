76078968a47954233b4c49148085d1852fa11bb2
bg1bgst333 <bg1bgst333@gmail.com>
2024-07-15 00:50:56 +0900

    add CFile::~CFile

---
+++ winapi/CFile/~CFile/src/CFile/CFile/main.cpp
@@ -0,0 +1,28 @@
+// ヘッダファイルのインクルード
+// 既定のヘッダ
+#include <windows.h>	// 標準WindowsAPI
+#include <tchar.h>		// TCHAR型
+#include <stdio.h>		// 標準入出力
+// 独自のヘッダ
+#include "File.h"		// CFile
+
+// _tmain関数の定義
+int _tmain(int argc, TCHAR* argv[]) {	// main関数のTCHAR版.
+
+	// CFileオブジェクトの生成.
+	CFile* pFile = new CFile();
+	BOOL bRet = pFile->Open(_T("test.txt"), GENERIC_READ, OPEN_EXISTING);	// CFile::Openで"test.txt"を読み込みモード開く.
+	if (bRet) {	// 成功.
+
+		// 成功のメッセージ表示.
+		_tprintf(_T("CFile::Open(test.txt) Success!\n"));
+
+	}
+
+	// CFileオブジェクトの破棄.
+	delete pFile;
+
+	// プログラムの終了.
+	return 0;	// 0を返して正常終了.
+
+}
