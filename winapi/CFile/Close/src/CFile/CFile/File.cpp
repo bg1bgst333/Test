935016daa6dbaa24cbb50adfb2dd37275b3d223f
bg1bgst333 <bg1bgst333@gmail.com>
2024-07-15 00:18:20 +0900

    add CFile::Close

---
+++ winapi/CFile/Close/src/CFile/CFile/File.cpp
@@ -0,0 +1,62 @@
+// ヘッダのインクルード
+// 既定のヘッダ
+#include <shlwapi.h>	// シェルAPI
+// 独自ヘッダ
+#include "File.h"	// CFile
+
+// ファイルを開く.
+BOOL CFile::Open(LPCTSTR lpctszFileName, DWORD dwDesiredAccess, DWORD dwCreationDisposition) {
+
+	// 与えられた引数のパスが相対パスかどうかチェック.
+	BOOL bRet = PathIsRelative(lpctszFileName);	// PathIsRelativeで相対パスかチェック.
+	if (bRet) {
+		// 絶対パスの取得.
+		TCHAR tszFullPath[1024] = { 0 };
+		TCHAR* ptszFilePart = NULL;
+		DWORD dwRet = GetFullPathName(lpctszFileName, 1024, tszFullPath, &ptszFilePart);	// GetFullPathNameでフルパスを取得.
+		if (dwRet) {
+			m_tstrFilePath = tszFullPath;
+			// ファイルハンドルの取得.
+			m_hFile = CreateFile(m_tstrFilePath.c_str(), dwDesiredAccess, FILE_SHARE_READ, NULL, dwCreationDisposition, FILE_ATTRIBUTE_NORMAL, NULL);	// CreateFileで開く.
+			if (m_hFile == INVALID_HANDLE_VALUE) {	// 失敗.
+				m_hFile = NULL;
+				m_tstrFilePath.clear();
+				return FALSE;
+			}
+			else {	// 成功.
+				return TRUE;
+			}
+		}
+		else {
+			return FALSE;
+		}
+	}
+	else {
+		m_tstrFilePath = lpctszFileName;
+		// ファイルハンドルの取得.
+		m_hFile = CreateFile(m_tstrFilePath.c_str(), dwDesiredAccess, FILE_SHARE_READ, NULL, dwCreationDisposition, FILE_ATTRIBUTE_NORMAL, NULL);	// CreateFileで開く.
+		if (m_hFile == INVALID_HANDLE_VALUE) {	// 失敗.
+			m_hFile = NULL;
+			m_tstrFilePath.clear();
+			return FALSE;
+		}
+		else {	// 成功.
+			return TRUE;
+		}
+	}
+
+}
+
+// ファイルを閉じる.
+void CFile::Close() {
+
+	// ファイルハンドルを閉じる.
+	if (m_hFile != NULL) {	// ハンドルが残っていたら.
+		CloseHandle(m_hFile);	// CloseHandleでm_hFileを閉じる.
+		m_hFile = NULL;	// m_hFileにNULLをセット.
+	}
+
+	// ファイル名などの情報もクリア.
+	m_tstrFilePath.clear();
+
+}
---
+++ winapi/CFile/Close/src/CFile/CFile/main.cpp
@@ -0,0 +1,31 @@
+// ヘッダファイルのインクルード
+// 既定のヘッダ
+#include <windows.h>	// 標準WindowsAPI
+#include <tchar.h>		// TCHAR型
+#include <stdio.h>		// 標準入出力
+// 独自のヘッダ
+#include "File.h"		// CFile
+
+// _tmain関数の定義
+int _tmain(int argc, TCHAR* argv[]) {	// main関数のTCHAR版.
+
+	// CFileオブジェクトの生成.
+	CFile* pFile = new CFile();
+	BOOL bRet = pFile->Open(_T("test.txt"), GENERIC_READ, OPEN_EXISTING);	// CFile::Openで"test.txt"を読み込みモード開く.
+	if (bRet) {	// 成功.
+
+		// 成功のメッセージ表示.
+		_tprintf(_T("CFile::Open(test.txt) Success!\n"));
+
+		// ファイルハンドルを閉じる.
+		pFile->Close();	// CFile::Closeで閉じる.
+
+	}
+
+	// CFileオブジェクトの破棄.
+	delete pFile;
+
+	// プログラムの終了.
+	return 0;	// 0を返して正常終了.
+
+}
