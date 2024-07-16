e00968ce995a70582083094aae389d21ab40a39f
bg1bgst333 <bg1bgst333@gmail.com>
2024-07-16 11:34:04 +0900

    add CBinaryFile::Set

---
+++ winapi/CBinaryFile/Set/src/CBinaryFile/CBinaryFile/BinaryFile.cpp
@@ -0,0 +1,18 @@
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "BinaryFile.h"	// CBinaryFile
+
+// 全部一斉に書き込むバッファのセット.
+void CBinaryFile::Set(BYTE *pBytes, DWORD dwSize){
+
+	// メンバにセット.
+	m_dwSize = dwSize;	// m_deSizeにdwSizeをセット.
+
+	// バッファの作成.
+	m_pBytes = new BYTE[m_dwSize + 1];	// newでm_dwSize + 1のバイト列を作成し, ポインタをm_pBytesに格納.
+	ZeroMemory(m_pBytes, m_dwSize + 1);	// ZeroMemoryでm_pBytesを(m_dwSize + 1)分, 0で埋める.
+
+	// データをコピー.
+	CopyMemory(m_pBytes, pBytes, m_dwSize);	// CopyMemoryでコピー.
+
+}
