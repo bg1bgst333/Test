6068ba0bb6b76aa3d5d3193e65d933d86756566e
bg1bgst333 <bg1bgst333@gmail.com>
2024-08-08 13:12:26 +0900

    add CTextFile::Write

---
+++ winapi/CTextFile/Write/src/CTextFile/CTextFile/TextFile.h
@@ -0,0 +1,24 @@
+// 二重インクルード防止
+#ifndef __TEXT_FILE_H__
+#define __TEXT_FILE_H__
+
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "BinaryFile.h"	// CBinaryFile
+
+// テキストファイルクラスCTextFile
+class CTextFile : public CBinaryFile {
+
+	// publicメンバ
+	public:
+
+		// publicメンバ変数
+		tstring m_tstrText;		// テキスト内容.
+
+		// publicメンバ関数
+		void SetText(tstring tstrText);	// テキストのセット.
+		virtual BOOL Write(LPCTSTR lpctszFileName);	// 指定のテキストファイルに全部一斉書き込み.
+
+};
+
+#endif
