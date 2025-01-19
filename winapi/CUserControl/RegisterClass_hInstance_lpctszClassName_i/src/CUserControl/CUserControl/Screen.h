2bc71c8c427baacc23f1e794a9689424c29dc37b
bg1bgst333 <bg1bgst333@gmail.com>
2025-01-19 12:51:40 +0900

    add CUserControl::RegisterClass_hInstance_lpctszClassName_i

---
+++ winapi/CUserControl/RegisterClass_hInstance_lpctszClassName_i/src/CUserControl/CUserControl/Screen.h
@@ -0,0 +1,27 @@
+// 二重インクルード防止
+#ifndef __SCREEN_H__
+#define __SCREEN_H__
+
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "UserControl.h"	// CUserControl
+
+// スクリーンクラスCScreen
+class CScreen : public CUserControl{
+
+	// publicメンバ
+	public:
+
+		// publicメンバ関数
+		// コンストラクタ・デストラクタ
+		CScreen();	// コンストラクタCScreen
+		virtual ~CScreen();	// デストラクタ~CScreen
+		// staticメンバ関数
+		static BOOL RegisterClass(HINSTANCE hInstance);	// ウィンドウクラス登録関数RegisterClass.(ウィンドウクラス名省略バージョン.)
+		// メンバ関数
+		virtual BOOL Create(LPCTSTR lpctszWindowName, DWORD dwStyle, int x, int y, int iWidth, int iHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance);	// ウィンドウ作成関数Create.(ウィンドウクラス名省略バージョン.)
+		virtual BOOL Destroy();	// ウィンドウ破棄関数Destroy
+
+};
+
+#endif
