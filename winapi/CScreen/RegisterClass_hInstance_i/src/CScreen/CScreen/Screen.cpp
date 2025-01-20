91289b618f5bb80678725556284c20a91acfec8e
bg1bgst333 <bg1bgst333@gmail.com>
2025-01-20 13:50:59 +0900

    add CScreen::RegisterClass_hInstance_i

---
+++ winapi/CScreen/RegisterClass_hInstance_i/src/CScreen/CScreen/Screen.cpp
@@ -0,0 +1,47 @@
+// ヘッダのインクルード
+// 既定のヘッダ
+#include <stdio.h>	// C標準入出力
+// 独自のヘッダ
+#include "Screen.h"	// CScreen
+
+// コンストラクタCScreen
+CScreen::CScreen() : CUserControl() {
+
+}
+
+// デストラクタ~CScreen
+CScreen::~CScreen() {
+
+}
+
+// ウィンドウクラス登録関数RegisterClass.(ウィンドウクラス名省略バージョン.)
+BOOL CScreen::RegisterClass(HINSTANCE hInstance) {
+
+	// ウィンドウクラスの登録.
+	return CUserControl::RegisterClass(hInstance, _T("CScreen"));	// CUserControl::RegisterClassでウィンドウクラス名"CScreen"を登録.
+
+}
+
+// ウィンドウクラス登録関数RegisterClass(背景色指定バージョン.)
+BOOL CScreen::RegisterClass(HINSTANCE hInstance, int i) {
+
+	// ウィンドウクラスの登録.
+	return CUserControl::RegisterClass(hInstance, _T("CScreen"), i);	// CUserControl::RegisterClassでウィンドウクラス名"CScreen"を登録.
+
+}
+
+// ウィンドウ作成関数Create.(ウィンドウクラス名省略バージョン.)
+BOOL CScreen::Create(LPCTSTR lpctszWindowName, DWORD dwStyle, int x, int y, int iWidth, int iHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance) {
+
+	// ウィンドウクラス名は"CScreen".
+	return CUserControl::Create(_T("CScreen"), lpctszWindowName, dwStyle, x, y, iWidth, iHeight, hWndParent, hMenu, hInstance);	// CUserControl::Createにウィンドウクラス名"CScreen"を指定.
+
+}
+
+// ウィンドウ破棄関数Destroy
+BOOL CScreen::Destroy() {
+
+	// 親クラスのDestroyを呼ぶ.
+	return CUserControl::Destroy();	// CUserControl::Destroyを呼ぶ.
+
+}
