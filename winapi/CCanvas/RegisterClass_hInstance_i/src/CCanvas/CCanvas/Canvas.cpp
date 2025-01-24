27a1cb00c457f13606f99623c4ebc2853e37c749
bg1bgst333 <bg1bgst333@gmail.com>
2025-01-25 03:05:22 +0900

    add CCanvas::RegisterClass_hInstance_i

---
+++ winapi/CCanvas/RegisterClass_hInstance_i/src/CCanvas/CCanvas/Canvas.cpp
@@ -0,0 +1,47 @@
+// ヘッダのインクルード
+// 既定のヘッダ
+#include <stdio.h>	// C標準入出力
+// 独自のヘッダ
+#include "Canvas.h"	// CCanvas
+
+// コンストラクタCCanvas
+CCanvas::CCanvas() : CUserControl(){
+
+}
+
+// デストラクタ~CCanvas
+CCanvas::~CCanvas(){
+
+}
+
+// ウィンドウクラス登録関数RegisterClass.(ウィンドウクラス名省略バージョン.)
+BOOL CCanvas::RegisterClass(HINSTANCE hInstance){
+
+	// ウィンドウクラスの登録.
+	return CUserControl::RegisterClass(hInstance, _T("CCanvas"));	// CUserControl::RegisterClassでウィンドウクラス名"CCanvas"を登録.
+
+}
+
+// ウィンドウクラス登録関数RegisterClass(背景色指定バージョン.)
+BOOL CCanvas::RegisterClass(HINSTANCE hInstance, int i){
+
+	// ウィンドウクラスの登録.
+	return CUserControl::RegisterClass(hInstance, _T("CCanvas"), i);	// CUserControl::RegisterClassでウィンドウクラス名"CCanvas"を登録.
+
+}
+
+// ウィンドウ作成関数Create.(ウィンドウクラス名省略バージョン.)
+BOOL CCanvas::Create(LPCTSTR lpctszWindowName, DWORD dwStyle, int x, int y, int iWidth, int iHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance){
+
+	// ウィンドウクラス名は"CCanvas".
+	return CUserControl::Create(_T("CCanvas"), lpctszWindowName, dwStyle, x, y, iWidth, iHeight, hWndParent, hMenu, hInstance);	// CUserControl::Createにウィンドウクラス名"CCanvas"を指定.
+	
+}
+
+// ウィンドウ破棄関数Destroy
+BOOL CCanvas::Destroy(){
+
+	// 親クラスのDestroyを呼ぶ.
+	return CUserControl::Destroy();	// CUserControl::Destroyを呼ぶ.
+
+}
---
+++ winapi/CCanvas/RegisterClass_hInstance_i/src/CCanvas/CCanvas/Screen.h
@@ -0,0 +1,34 @@
+// 二重インクルード防止
+#ifndef __SCREEN_H__
+#define __SCREEN_H__
+
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "UserControl.h"	// CUserControl
+#include "Canvas.h"	// CCanvas
+
+// スクリーンクラスCScreen
+class CScreen : public CUserControl{
+
+	// publicメンバ
+	public:
+
+		// メンバ変数
+		CCanvas *m_pCanvas;	// CCanvasオブジェクトポインタm_pCanvas.
+
+		// publicメンバ関数
+		// コンストラクタ・デストラクタ
+		CScreen();	// コンストラクタCScreen
+		virtual ~CScreen();	// デストラクタ~CScreen
+		// staticメンバ関数
+		static BOOL RegisterClass(HINSTANCE hInstance);	// ウィンドウクラス登録関数RegisterClass.(ウィンドウクラス名省略バージョン.)
+		static BOOL RegisterClass(HINSTANCE hInstance, int i);	// ウィンドウクラス登録関数RegisterClass(背景色指定バージョン.)
+		// メンバ関数
+		virtual BOOL Create(LPCTSTR lpctszWindowName, DWORD dwStyle, int x, int y, int iWidth, int iHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance);	// ウィンドウ作成関数Create.(ウィンドウクラス名省略バージョン.)
+		virtual BOOL Destroy();	// ウィンドウ破棄関数Destroy
+		virtual BOOL DestroyChildren();	// 子ウィンドウ破棄関数DestroyChildren
+		virtual int OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct);	// ウィンドウの作成が開始された時.
+
+};
+
+#endif
