7be482a73f280f6febf4437ba9d584526534591d
bg1bgst333 <bg1bgst333@gmail.com>
2025-01-28 16:18:03 +0900

    add CDIBSection

---
+++ winapi/CDIBSection/CDIBSection/src/CDIBSection/CDIBSection/DIBSection.h
@@ -0,0 +1,28 @@
+// 二重インクルード防止
+#ifndef __DIBSECTION_H__
+#define __DIBSECTION_H__
+
+// ヘッダのインクルード
+// 既定のヘッダ
+#include <windows.h>	// 標準WindowsAPI
+
+// DIBセクションクラスCDIBSection
+class CDIBSection {
+
+	// publicメンバ
+	public:
+
+		// publicメンバ変数
+		HBITMAP m_hDIBSection;	// HBITMAP型DIBセクションハンドルm_hDIBSection.
+		LPBYTE m_lpBitsPixel;	// LPBYTE型DIBセクションピクセル列m_lpBitsPixel.
+
+		// publicメンバ関数
+		// コンストラクタ・デストラクタ
+		CDIBSection();	// コンストラクタCDIBSection()
+		virtual ~CDIBSection();	// デストラクタ~CDIBSection()
+		// メンバ関数
+		virtual BOOL CreateDIBSection(HWND hWnd, int iWidth, int iHeight);	// DIBセクション作成関数CreateDIBSection.
+
+};
+
+#endif
