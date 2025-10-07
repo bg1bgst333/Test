ed3a4b143e85cd2e579a1775aee0874ec8911033
bg1bgst333 <bg1bgst333@gmail.com>
2025-10-07 21:25:28 +0900

    add CMultiView

---
+++ winapi/CMultiView/CMultiView/src/CMultiView/CMultiView/MultiView.cpp
@@ -0,0 +1,50 @@
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "MultiView.h"	// CMultiView
+
+// コンストラクタCMultiView
+CMultiView::CMultiView() : CUserControl() {
+
+}
+
+// デストラクタ~CMultiView
+CMultiView::~CMultiView() {
+
+}
+
+// ウィンドウクラス登録関数RegisterClass.
+BOOL CMultiView::RegisterClass(HINSTANCE hInstance) {
+
+	// ウィンドウクラスの登録.
+	return CUserControl::RegisterClass(hInstance, _T("CMultiView"));	// CUserControl::RegisterClassで登録.
+
+}
+
+// ウィンドウ作成関数Create.
+BOOL CMultiView::Create(LPCTSTR lpctszWindowName, DWORD dwStyle, int x, int y, int iWidth, int iHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance) {
+
+	// ウィンドウの作成.
+	return CUserControl::Create(_T("CMultiView"), lpctszWindowName, dwStyle | WS_CHILD | WS_VISIBLE, x, y, iWidth, iHeight, hWndParent, hMenu, hInstance);	// CWindow::Createで作成.
+
+}
+
+// ウィンドウの作成が開始された時.
+int CMultiView::OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct) {
+
+	// 常にウィンドウ作成に成功するものとする.
+	return 0;	// 0を返すと, ウィンドウ作成に成功したということになる.
+
+}
+
+// ウィンドウが破棄された時.
+void CMultiView::OnDestroy() {
+
+	// 親クラスのOnDestroyを呼ぶ.
+	CUserControl::OnDestroy();	// CUserControl::OnDestroyを呼ぶ.
+
+}
+
+// ウィンドウのサイズが変更された時.
+void CMultiView::OnSize(UINT nType, int cx, int cy) {
+
+}
