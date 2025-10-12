7e242aa299ebb31e0ae4e4f3776e859b51162579
bg1bgst333 <bg1bgst333@gmail.com>
2025-10-12 21:02:43 +0900

    add CMultiView::RegisterClass_hInstance_hbrBackground

---
+++ winapi/CMultiView/RegisterClass_hInstance_hbrBackground/src/CMultiView/CMultiView/MultiView.h
@@ -0,0 +1,29 @@
+// 二重インクルード防止
+#ifndef __MULTI_VIEW_H__
+#define __MULTI_VIEW_H__
+
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "UserControl.h"	// CUserControl
+
+// マルチビューコントロールクラスCMultiView
+class CMultiView : public CUserControl {
+
+	// publicメンバ
+	public:
+
+		// publicメンバ関数
+		// コンストラクタ・デストラクタ
+		CMultiView();	// コンストラクタCMultiView
+		virtual ~CMultiView();	// デストラクタ~CMultiView
+		// staticメンバ関数
+		static BOOL RegisterClass(HINSTANCE hInstance);	// ウィンドウクラス登録関数RegisterClass.
+		// メンバ関数
+		virtual BOOL Create(LPCTSTR lpctszWindowName, DWORD dwStyle, int x, int y, int iWidth, int iHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance);	// ウィンドウ作成関数Create.
+		virtual int OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct);	// ウィンドウの作成が開始された時.
+		virtual void OnDestroy();	// ウィンドウが破棄された時.
+		virtual void OnSize(UINT nType, int cx, int cy);	// ウィンドウのサイズが変更された時.
+
+};
+
+#endif
