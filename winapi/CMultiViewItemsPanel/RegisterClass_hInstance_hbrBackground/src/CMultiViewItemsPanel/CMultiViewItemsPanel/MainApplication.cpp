30f02eafe66d35e2312b45d46849ca6e2d52dab4
bg1bgst333 <bg1bgst333@gmail.com>
2025-10-22 09:57:02 +0900

    add CMultiViewItemsPanel::RegisterClass_hInstance_hbrBackground

---
+++ winapi/CMultiViewItemsPanel/RegisterClass_hInstance_hbrBackground/src/CMultiViewItemsPanel/CMultiViewItemsPanel/MainApplication.cpp
@@ -0,0 +1,46 @@
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "resource.h"
+#include "MainApplication.h"	// CMainApplication
+#include "MainWindow.h"			// CMainWindow
+
+// インスタンス初期化関数InitInstance.
+BOOL CMainApplication::InitInstance(HINSTANCE hInstance, LPTSTR lpCmdLine, int nShowCmd) {
+
+	// ウィンドウクラスの登録.
+	CMainWindow::RegisterClass(hInstance);
+	m_hBrush1 = CreateSolidBrush(RGB(0xff, 0, 0));	// 赤ブラシの生成.
+	CMultiView::RegisterClass(hInstance, m_hBrush1);	// 赤いCMultiViewの登録.
+	m_hBrush2 = CreateSolidBrush(RGB(0, 0, 0xff));	// 青ブラシの生成.
+	CMultiViewItemsPanel::RegisterClass(hInstance, m_hBrush2);	// 青いCMultiViewItemsPanelの登録.
+
+	// CMainWindowオブジェクトの作成.
+	m_pMainWnd = new CMainWindow();	// CMainWindowオブジェクトを作成し, m_pMainWndに格納.
+
+	// ウィンドウの作成.
+	if (!m_pMainWnd->Create(_T("CMultiViewItemsPanel"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance)) {	// m_pMainWnd->Createでウィンドウ作成し, 失敗した場合.
+
+		// エラー処理
+		return FALSE;	// returnでFALSEを返して異常終了.
+
+	}
+
+	// ウィンドウの表示.
+	m_pMainWnd->ShowWindow(SW_SHOW);	// m_pMainWnd->ShowWindowで表示.
+
+	// TRUEを返す.
+	return TRUE;	// returnでTRUEを返す.
+
+}
+
+// 終了処理関数ExitInstance.
+int CMainApplication::ExitInstance() {
+
+	// ブラシの解放.
+	DeleteObject(m_hBrush2);
+	DeleteObject(m_hBrush1);
+
+	// 親クラスのExitInstanceを呼ぶ.
+	return CApplication::ExitInstance();
+
+}
