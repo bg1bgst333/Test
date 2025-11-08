87522c9b48ac9716cff94a848bae5bcd507b7075
bg1bgst333 <bg1bgst333@gmail.com>
2025-11-08 10:18:25 +0900

    add CMultiViewItemsPanel::Destroy

---
+++ winapi/CMultiViewItemsPanel/Destroy/src/CMultiViewItemsPanel/CMultiViewItemsPanel/resource.h
@@ -0,0 +1,7 @@
+#include <windows.h>
+
+#define IDM_MAINMENU			100
+#define ID_ITEM_FILE_OPEN		10101
+#define IDC_MULTIVIEW			201
+#define IDC_MULTIVIEWITEMSPANEL	202
+#define IDC_MULTIVIEWITEM		203
