a16e5693e105ae8d25778146427ecce84b36a736
bg1bgst333 <bg1bgst333@gmail.com>
2025-11-26 11:02:57 +0900

    add CComboBox

---
+++ winapi/CComboBox/CComboBox/src/CComboBox/CComboBox/ComboBox.h
@@ -0,0 +1,22 @@
+// 二重インクルード防止
+#ifndef __COMBO_BOX_H__
+#define __COMBO_BOX_H__
+
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "CustomControl.h"	// CCustomControl
+
+// コンボボックスコントロールクラス
+class CComboBox : public CCustomControl{
+
+	// publicメンバ
+	public:
+
+		// publicメンバ巻数
+		// コンストラクタ・デストラクタ
+		CComboBox();	// コンストラクタCComboBox
+		virtual ~CComboBox();	// デストラクタ~CComboBox
+
+};
+
+#endif
