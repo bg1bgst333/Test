c2d449ce7b929d69d5bd94b6bbb9659b76d34af7
bg1bgst333 <bg1bgst333@gmail.com>
2025-11-30 10:46:56 +0900

    add CComboBox::AddString

---
+++ winapi/CComboBox/AddString/src/CComboBox/CComboBox/ComboBox.cpp
@@ -0,0 +1,32 @@
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "ComboBox.h"	// CComboBox
+
+// コンストラクタCComboBox
+CComboBox::CComboBox() : CCustomControl(){
+
+}
+
+// デストラクタ~CComboBox
+CComboBox::~CComboBox(){
+
+	// メンバの終了処理
+	Destroy();	// Destroyでこのウィンドウの終了処理をする.
+
+}
+
+// ウィンドウ作成関数Create.
+BOOL CComboBox::Create(LPCTSTR lpctszWindowName, DWORD dwStyle, int x, int y, int iWidth, int iHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance){
+
+	// ウィンドウクラス名には"ComboBox"を指定.
+	return CCustomControl::Create(_T("ComboBox"), lpctszWindowName, dwStyle | ES_MULTILINE | ES_WANTRETURN | ES_AUTOHSCROLL | ES_AUTOVSCROLL, x, y, iWidth, iHeight, hWndParent, hMenu, hInstance);	// CCustomControl::Createにウィンドウクラス名"ComboBox"を指定.
+
+}
+
+// コンボボックスに文字列を追加する関数AddString.
+void CComboBox::AddString(LPCTSTR lpctszStr){
+
+	// 指定の文字列を追加.
+	SendMessage(m_hWnd, CB_ADDSTRING, 0, (LPARAM)lpctszStr);	// SendMessageでCB_ADDSTRINGを送ることで指定の文字列を追加.
+
+}
