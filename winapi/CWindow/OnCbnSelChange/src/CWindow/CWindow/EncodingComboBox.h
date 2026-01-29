387115c118ce2d8dada57fd6e152d4f70afa9acb
bg1bgst333 <bg1bgst333@gmail.com>
2026-01-29 21:31:33 +0900

    add CWindow::OnCbnSelChange

---
+++ winapi/CWindow/OnCbnSelChange/src/CWindow/CWindow/EncodingComboBox.h
@@ -0,0 +1,21 @@
+// 二重インクルード防止
+#ifndef __ENCODING_COMBO_BOX_H__
+#define __ENCODING_COMBO_BOX_H__
+
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "ComboBox.h"	// CComboBox
+
+// エンコーディングコンボボックスコントロールクラス
+class CEncodingComboBox : public CComboBox{
+
+	// publicメンバ
+	public:
+
+		// publicメンバ巻数
+		// メンバ関数
+		virtual void OnSize(UINT nType, int cx, int cy);	// ウィンドウのサイズが変更された時.
+
+};
+
+#endif
