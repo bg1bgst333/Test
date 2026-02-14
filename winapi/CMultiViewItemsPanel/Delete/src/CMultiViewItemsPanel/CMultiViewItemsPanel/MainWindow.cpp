d781b019ca9585482730695e989a9eef8c158f31
bg1bgst333 <bg1bgst333@gmail.com>
2026-02-14 21:45:21 +0900

    add CMultiViewItemsPanel::Delete

---
+++ winapi/CMultiViewItemsPanel/Delete/src/CMultiViewItemsPanel/CMultiViewItemsPanel/MainWindow.cpp
@@ -0,0 +1,162 @@
+// ヘッダのインクルード
+// 既定のヘッダ
+#include <windows.h>	// 標準WindowsAPI
+#include <stdio.h>	// C標準入出力
+#include <commctrl.h>	// コモンコントロール
+// 独自のヘッダ
+#include "MainWindow.h"	// CMainWindow
+#include "resource.h"
+
+// ウィンドウクラス登録関数RegisterClass.
+BOOL CMainWindow::RegisterClass(HINSTANCE hInstance){
+
+	// ウィンドウクラス名は"CMainWindow".
+	return CWindow::RegisterClass(hInstance, _T("CMainWindow"));	// CWindow::RegisterClassでウィンドウクラス名"CMainWindow"を登録.
+
+}
+
+// ウィンドウクラス登録関数RegisterClass.(メニュー名指定バージョン)
+BOOL CMainWindow::RegisterClass(HINSTANCE hInstance, LPCTSTR lpctszMenuName){
+
+	// メニュー名はlpctszMenuName.
+	return CWindow::RegisterClass(hInstance, _T("CMainWindow"), lpctszMenuName);	// CWindow::RegisterClassで, ウィンドウクラス名"CMainWindow", メニュー名lpctszMenuNameを登録.
+
+}
+
+// コンストラクタCMainWindow()
+CMainWindow::CMainWindow(){
+
+	// メンバの初期化.
+	m_hInstance = NULL;
+	m_pMainMenu = NULL;
+	m_pWindowListView = NULL;
+
+}
+
+// デストラクタ~CMainWindow()
+CMainWindow::~CMainWindow(){
+
+	// メンバの終了処理.
+	Destroy();	// Destroyで子ウィンドウの破棄.
+
+}
+
+// ウィンドウ作成関数Create.(ウィンドウクラス名省略バージョン.)
+BOOL CMainWindow::Create(LPCTSTR lpctszWindowName, DWORD dwStyle, int x, int y, int iWidth, int iHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance){
+
+	// ウィンドウクラス名は"CMainWindow".
+	return CWindow::Create(_T("CMainWindow"), lpctszWindowName, dwStyle, x, y, iWidth, iHeight, hWndParent, hMenu, hInstance);	// CWindow::Createにウィンドウクラス名"CMainWindow"を指定.
+
+}
+
+// ウィンドウ作成関数CreateEx.
+BOOL CMainWindow::CreateEx(DWORD dwExStyle, LPCTSTR lpctszWindowName, DWORD dwStyle, int x, int y, int iWidth, int iHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance){
+
+	// ウィンドウクラス名は"CMainWindow".
+	return CWindow::CreateEx(dwExStyle, _T("CMainWindow"), lpctszWindowName, dwStyle, x, y, iWidth, iHeight, hWndParent, hMenu, hInstance);	// CWindow::CreateExにウィンドウクラス名"CMainWindow"を指定.
+
+}
+
+// ウィンドウ破棄関数Destroy
+BOOL CMainWindow::Destroy(){
+
+	// 変数の初期化.
+	BOOL bRet = FALSE;	// bRetをFALSEで初期化.
+
+	// DestroyChildrenを分けたので, 自身のウィンドウ破棄は問題ない.
+	// まず子ウィンドウの破棄.
+	DestroyChildren();
+
+	// 自身のウィンドウ破棄.
+	bRet = CWindow::Destroy();	// 戻り値をbRetに格納.
+
+	// bRetを返す.
+	return bRet;
+
+}
+
+// 子ウィンドウ破棄関数DestroyChildren
+BOOL CMainWindow::DestroyChildren(){
+
+	// 変数の初期化.
+	BOOL bRet = FALSE;	// bRetをFALSEで初期化.
+
+	// メンバの終了処理.
+	if (m_pWindowListView != NULL){
+		bRet = m_pWindowListView->Destroy();
+		delete m_pWindowListView;
+		m_pWindowListView = NULL;
+	}
+
+	// 破棄したらTRUEを返す.
+	if (bRet) {	// TRUEなら.
+		return TRUE;	// TRUEを返す.
+	}
+
+	// 破棄しなければ, CWindowのDestroyChildrenを返す.
+	return CWindow::DestroyChildren();	// CWindow::DestroyChildrenを返す.
+
+}
+
+// ウィンドウの作成が開始された時.
+int CMainWindow::OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct){
+
+	// 親クラスのOnCreateを呼ぶ.
+	m_hInstance = lpCreateStruct->hInstance;
+	int iRet = CWindow::OnCreate(hwnd, lpCreateStruct);	// CWindow::OnCreateを呼び, 戻り値をiRetに格納.
+	// CWindowListViewの生成.
+	RECT rc = {0};
+	GetClientRect(hwnd, &rc);
+	m_pWindowListView = new CWindowListView();
+	m_pWindowListView->Create(_T(""), 0, 0, 0, rc.right - rc.left, rc.bottom - rc.top, hwnd, (HMENU)IDC_WINDOWLISTVIEW, m_hInstance);	
+	// アイテムの挿入.
+	m_pWindowListView->m_pMultiViewItemsPanel->Insert(0, _T("Item1"), _T("Item1"), 50, 50, 100, 100, m_hInstance);
+	m_pWindowListView->m_pMultiViewItemsPanel->Insert(1, _T("Item2"), _T("Item2"), 160, 50, 100, 100, m_hInstance);
+	m_pWindowListView->m_pMultiViewItemsPanel->Insert(1, _T("Item3"), _T("Item3"), 270, 50, 100, 100, m_hInstance);
+	// アイテムの順番をデバッグ確認.
+	OutputDebugString(m_pWindowListView->m_pMultiViewItemsPanel->Get(0)->m_tstrKey.c_str());
+	OutputDebugString(_T("\n"));
+	OutputDebugString(m_pWindowListView->m_pMultiViewItemsPanel->Get(1)->m_tstrKey.c_str());
+	OutputDebugString(_T("\n"));
+	OutputDebugString(m_pWindowListView->m_pMultiViewItemsPanel->Get(2)->m_tstrKey.c_str());
+	OutputDebugString(_T("\n"));
+	// アイテムの削除.
+	m_pWindowListView->m_pMultiViewItemsPanel->Delete(0);
+	// アイテムの順番をデバッグ確認.
+	OutputDebugString(m_pWindowListView->m_pMultiViewItemsPanel->Get(0)->m_tstrKey.c_str());
+	OutputDebugString(_T("\n"));
+	OutputDebugString(m_pWindowListView->m_pMultiViewItemsPanel->Get(1)->m_tstrKey.c_str());
+	OutputDebugString(_T("\n"));
+	return iRet;	// iRetを返す.
+
+}
+
+// ウィンドウが破棄された時.
+void CMainWindow::OnDestroy(){
+
+	// CWindowのOnDestroyを呼ぶ.
+	CWindow::OnDestroy();	// CWindow::OnDestroyを呼ぶ.
+
+}
+
+// ウィンドウのサイズが変更された時.
+void CMainWindow::OnSize(UINT nType, int cx, int cy){
+
+}
+
+// ウィンドウが閉じられる時.
+int CMainWindow::OnClose(){
+
+	// メッセージボックスで"Close CMainWindow OK?"と表示.
+	int iRet = MessageBox(m_hWnd, _T("Close CMainWindow OK?"), _T("CMultiViewItemsPanel"), MB_OKCANCEL);	// MessageBoxで"Close CMainWindow OK?"と表示し, 戻り値をiRetに格納.
+	if (iRet != IDOK){	// OK以外.(Cancelなど.)
+		return -1;	// -1を返す.
+	}
+
+	// このウィンドウの破棄.(OnCloseの後, ウィンドウの破棄処理が勝手に行われるので, Destroyは不要なのでコメントアウト.)
+	//Destroy();	// Destroyでこのウィンドウの破棄処理.
+
+	// OKなので閉じる.
+	return CWindow::OnClose();	// 親クラスのOnCloseを呼ぶ.(親クラスのOnCloseは常に閉じる処理になっている.)
+
+}
