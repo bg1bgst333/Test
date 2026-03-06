d4c5093183e916b7a038ed363a8d001dfbbdca47
bg1bgst333 <bg1bgst333@gmail.com>
2026-03-06 10:13:25 +0900

    add CGraphicalApplication::OnIdle

---
+++ winapi/CGraphicalApplication/OnIdle/src/CGraphicalApplication/CGraphicalApplication/GraphicalApplication.h
@@ -0,0 +1,24 @@
+// 二重インクルード防止
+#ifndef __GRAPHICAL_APPLICATION_H__
+#define __GRAPHICAL_APPLICATION_H__
+
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "Application.h"	// CApplication
+
+// グラフィカルアプリケーションクラスCGraphicalApplication
+class CGraphicalApplication : public CApplication {
+
+	// publicメンバ
+	public:
+
+		// publicメンバ関数
+		// コンストラクタ
+		CGraphicalApplication();	// コンストラクタCGraphicalApplication
+		virtual int Run();	// メッセージループ処理関数Run.
+		virtual int ExitInstance();	// 終了処理関数ExitInstance.
+		virtual BOOL OnIdle(LONG lCount);	// アイドル処理関数OnIdle.
+
+};
+
+#endif
---
+++ winapi/CGraphicalApplication/OnIdle/src/CGraphicalApplication/CGraphicalApplication/MainApplication.cpp
@@ -0,0 +1,29 @@
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "MainApplication.h"	// CMainApplication
+#include "MainWindow.h"			// CMainWindow
+
+// インスタンス初期化関数InitInstance.
+BOOL CMainApplication::InitInstance(HINSTANCE hInstance, LPTSTR lpCmdLine, int nShowCmd) {
+
+	// ウィンドウクラスの登録.
+	CMainWindow::RegisterClass(hInstance);
+
+	// CMainWindowオブジェクトの作成.
+	CMainWindow* pMainWnd = new CMainWindow();	// CMainWindowオブジェクトを作成し, pMainWndに格納.
+	m_pMainWnd = pMainWnd;	// pMainWndをm_pMainWndにもセット.
+	// ウィンドウの作成.
+	if (!pMainWnd->CreateEx(0, _T("CGraphicalApplication"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance)) {	// m_pMainWnd->CreateExでウィンドウ作成し, 失敗した場合.
+
+		// エラー処理
+		return FALSE;	// returnでFALSEを返して異常終了.
+
+	}
+
+	// ウィンドウの表示.
+	m_pMainWnd->ShowWindow(SW_SHOW);	// m_pMainWnd->ShowWindowで表示.
+
+	// TRUEを返す.
+	return TRUE;	// returnでTRUEを返す.
+
+}
