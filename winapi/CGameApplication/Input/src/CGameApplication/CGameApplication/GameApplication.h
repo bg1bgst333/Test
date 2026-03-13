446289a21b9acdea8cd7e4f0de6fa1a3ab34f420
bg1bgst333 <bg1bgst333@gmail.com>
2026-03-13 10:06:07 +0900

    add CGameApplication::Input

---
+++ winapi/CGameApplication/Input/src/CGameApplication/CGameApplication/GameApplication.h
@@ -0,0 +1,27 @@
+// 二重インクルード防止
+#ifndef __GAME_APPLICATION_H__
+#define __GAME_APPLICATION_H__
+
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "GraphicalApplication.h"	// CGraphicalApplication
+
+// ゲームアプリケーションクラスCGameApplication
+class CGameApplication : public CGraphicalApplication {
+
+	// publicメンバ
+	public:
+
+		// publicメンバ変数
+		int m_x;	// テキストの位置X座標m_x
+		int m_y;	// テキストの位置Y座標m_y
+
+		// publicメンバ関数
+		// コンストラクタ
+		CGameApplication();	// コンストラクタCGameApplication
+		virtual void MainProc();	// メインループ処理関数MainProc.
+		virtual void Input();	// 入力処理関数Input.
+
+};
+
+#endif
