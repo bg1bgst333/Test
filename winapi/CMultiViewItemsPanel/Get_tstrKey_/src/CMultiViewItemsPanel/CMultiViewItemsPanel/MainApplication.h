9224d829a2c1ef4a91ab2881b7df49c432823a52
bg1bgst333 <bg1bgst333@gmail.com>
2026-08-20 12:34:07 +0900

    add CMultiViewItemsPanel::Get(key)

---
+++ winapi/CMultiViewItemsPanel/Get_tstrKey_/src/CMultiViewItemsPanel/CMultiViewItemsPanel/MainApplication.h
@@ -0,0 +1,29 @@
+// 二重インクルード防止
+#ifndef __MAIN_APPLICATION_H__
+#define __MAIN_APPLICATION_H__
+
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "Application.h"	// CApplication
+
+// メインアプリケーションクラスCMainApplication
+class CMainApplication : public CApplication{
+
+	// publicメンバ
+	public:
+
+		// publicメンバ変数
+		HBRUSH m_hBrush1;
+		HBRUSH m_hBrush2;
+		HBRUSH m_hBrush3;
+		HBITMAP m_hBitmap1;
+		HBITMAP m_hBitmap2;
+		HBITMAP m_hBitmap3;
+
+		// publicメンバ関数
+		virtual BOOL InitInstance(HINSTANCE hInstance, LPTSTR lpCmdLine, int nShowCmd);	// インスタンス初期化関数InitInstance.
+		virtual int ExitInstance();	// 終了処理関数ExitInstance.
+
+};
+
+#endif
