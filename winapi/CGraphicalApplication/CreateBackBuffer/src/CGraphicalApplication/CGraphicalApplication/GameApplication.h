9fb71e8645bf2204e98a3955fdd4c3ae78e7c7be
bg1bgst333 <bg1bgst333@gmail.com>
2026-09-13 17:02:33 +0900

    add CGraphicalApplication::CreateBackBuffer

---
+++ winapi/CGraphicalApplication/CreateBackBuffer/src/CGraphicalApplication/CGraphicalApplication/GameApplication.h
@@ -0,0 +1,31 @@
+// 二重インクルード防止
+#ifndef __GAME_APPLICATION_H__
+#define __GAME_APPLICATION_H__
+
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "GraphicalApplication.h"	// CGraphicalApplication
+
+// ゲームアプリケーションクラスCGameApplication
+class CGameApplication : public CGraphicalApplication {
+
+	// publicメンバ
+	public:
+
+		// publicメンバ変数
+		int m_x;	// テキストの位置X座標m_x
+		int m_y;	// テキストの位置Y座標m_y
+		int m_vx;	// X方向の速度m_vx;
+		int m_vy;	// Y方向の速度m_vy;
+
+		// publicメンバ関数
+		// コンストラクタ
+		CGameApplication();	// コンストラクタCGameApplication
+		virtual void MainProc();	// メインループ処理関数MainProc.
+		virtual void Input();	// 入力処理関数Input.
+		virtual void Logic();	// ロジック処理関数Logic.
+		virtual void Render();	// 描画処理関数Render.
+
+};
+
+#endif
