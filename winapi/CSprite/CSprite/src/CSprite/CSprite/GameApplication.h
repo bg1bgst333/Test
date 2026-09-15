7b1a6c82100ccdb84af84669f81db80a13b94bbe
bg1bgst333 <bg1bgst333@gmail.com>
2026-09-15 12:54:04 +0900

    add CSprite

---
+++ winapi/CSprite/CSprite/src/CSprite/CSprite/GameApplication.h
@@ -0,0 +1,33 @@
+// 二重インクルード防止
+#ifndef __GAME_APPLICATION_H__
+#define __GAME_APPLICATION_H__
+
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "GraphicalApplication.h"	// CGraphicalApplication
+#include "Sprite.h"	// CSprite
+
+// ゲームアプリケーションクラスCGameApplication
+class CGameApplication : public CGraphicalApplication{
+
+	// publicメンバ
+	public:
+
+		// publicメンバ変数
+		int m_x;	// テキストの位置X座標m_x
+		int m_y;	// テキストの位置Y座標m_y
+		int m_vx;	// X方向の速度m_vx;
+		int m_vy;	// Y方向の速度m_vy;
+		CSprite m_Sprite;	// CSpriteオブジェクトm_Sprite.
+
+		// publicメンバ関数
+		// コンストラクタ
+		CGameApplication();	// コンストラクタCGameApplication
+		virtual void MainProc();	// メインループ処理関数MainProc.
+		virtual void Input();	// 入力処理関数Input.
+		virtual void Logic();	// ロジック処理関数Logic.
+		virtual void Render();	// 描画処理関数Render.
+
+};
+
+#endif
---
+++ winapi/CSprite/CSprite/src/CSprite/CSprite/Sprite.cpp
@@ -0,0 +1,45 @@
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "Sprite.h"	// CSprite
+
+// コンストラクタCSprite()
+CSprite::CSprite(){
+
+	// メンバの初期化.
+	m_x = 0;	// m_xを0で初期化.
+	m_y = 0;	// m_yを0で初期化.
+
+}
+
+// デストラクタ~CSprite()
+CSprite::~CSprite(){
+
+}
+
+// 画像読込関数Load.
+BOOL CSprite::Load(HINSTANCE hInstance, LPCTSTR lpctszFileName){
+
+	// CBitmap::LoadImageでファイルから画像をロード.
+	return m_Bitmap.LoadImage(hInstance, lpctszFileName, IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);	// LR_LOADFROMFILEでlpctszFileNameのパスからロード.
+
+}
+
+// 描画関数Draw.
+void CSprite::Draw(HDC hDC){
+
+	// ビットマップのサイズを取得.
+	BITMAP bm = {0};
+	GetObject((HBITMAP)m_Bitmap, sizeof(BITMAP), &bm);	// GetObjectでビットマップの幅・高さ等を取得.
+
+	// 描画元のメモリDCを作成し, ビットマップを選択.
+	HDC hMemDC = CreateCompatibleDC(hDC);	// CreateCompatibleDCでhDCと互換のメモリDCを作成.
+	HBITMAP hOldBitmap = (HBITMAP)SelectObject(hMemDC, (HBITMAP)m_Bitmap);	// SelectObjectでメモリDCにビットマップを選択.
+
+	// BitBltで描画.
+	BitBlt(hDC, m_x, m_y, bm.bmWidth, bm.bmHeight, hMemDC, 0, 0, SRCCOPY);	// BitBltでhDCのm_x, m_y位置に描画.
+
+	// 後始末.
+	SelectObject(hMemDC, hOldBitmap);	// SelectObjectで元のビットマップに戻す.
+	DeleteDC(hMemDC);	// DeleteDCでメモリDCを破棄.
+
+}
