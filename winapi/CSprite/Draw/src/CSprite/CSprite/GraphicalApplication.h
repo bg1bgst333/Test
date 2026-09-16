7fd7117cd9ea4f4023f5fc3a9a75463e4ad9c8c8
bg1bgst333 <bg1bgst333@gmail.com>
2026-09-16 17:20:50 +0900

    add CSprite::Draw

---
+++ winapi/CSprite/Draw/src/CSprite/CSprite/GraphicalApplication.h
@@ -0,0 +1,33 @@
+// 二重インクルード防止
+#ifndef __GRAPHICAL_APPLICATION_H__
+#define __GRAPHICAL_APPLICATION_H__
+
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "Application.h"	// CApplication
+
+// グラフィカルアプリケーションクラスCGraphicalApplication
+class CGraphicalApplication : public CApplication{
+
+	// publicメンバ
+	public:
+
+		// publicメンバ変数
+		HDC m_hBackBufferDC;	// メモリデバイスコンテキストハンドルm_hBackBufferDC.
+		HBITMAP m_hBackBufferBitmap;	// バックバッファビットマップハンドルm_hBackBufferBitmap.
+		HBITMAP m_hOldBitmap;	// 以前のビットマップハンドルm_hOldBitmap.
+		int m_iBackBufferWidth;	// バックバッファの幅m_iBackBufferWidth.
+		int m_iBackBufferHeight;	// バックバッファの高さm_iBackBufferHeight.
+		// publicメンバ関数
+		// コンストラクタ
+		CGraphicalApplication();	// コンストラクタCGraphicalApplication
+		virtual int Run();	// メッセージループ処理関数Run.
+		virtual int ExitInstance();	// 終了処理関数ExitInstance.
+		virtual BOOL OnIdle(LONG lCount);	// アイドル処理関数OnIdle.
+		virtual void MainProc();	// メインループ処理関数MainProc.
+		virtual void CreateBackBuffer();	// バックバッファ作成関数CreateBackBuffer.
+		virtual void DestroyBackBuffer();	// バックバッファ破棄関数DestroyBackBuffer.
+
+};
+
+#endif
